96b11513b83c4ec651a926fc4551361fb78abf00
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-04 00:57:37 -0600

    Infra: Update leaderboard script, ignore local files around leaderboard

--- show_leaderboard.sh
+++ show_leaderboard.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 read -r -d  '' filter <<EOS
-.members | to_entries | sort_by(.value.local_score)[].value | { name, local_score, id, "cdls": .completion_day_level | map_values(. | map_values(.get_star_ts | tonumber | todate ))}
+.members
+| to_entries
+| sort_by(.value.local_score)[].value
+| {
+	name,
+	local_score,
+	id,
+	stars,
+	"last_star_ts": .last_star_ts | tonumber | todate,
+	"cdls": .completion_day_level | map_values(. | map_values(.get_star_ts | tonumber | todate ))
+}
 EOS
 jq --sort-keys "$filter"
