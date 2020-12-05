9fab3e3a55ac45af85f6f482c4beed22c233938f
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-04 23:30:20 -0600

    Day 5: Initial solution

--- src/advent_of_code_2020/day5.clj
+++ src/advent_of_code_2020/day5.clj
@@ -1,15 +1,29 @@
 (ns advent-of-code-2020.day5
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.set :as set]))
 
 (def day-5-input (io/resource "day5.txt"))
+
+(defn get-seat-id
+  [seat-string]
+  (Integer/parseInt
+    (str/replace
+      (str/replace seat-string #"[BR]" "1")
+      #"[FL]" "0")
+    2))
 
 (defn main-1
   []
   (println
-    (slurp day-5-input)))
+    (reduce (fn [max-so-far seat-string]
+              (max max-so-far (get-seat-id seat-string)))
+            0
+            (str/split-lines (slurp day-5-input)))))
 
 (defn main-2
   []
-  (println
-    (slurp day-5-input)))
+  (let [seat-ids (map get-seat-id (str/split-lines (slurp day-5-input)))
+        all-seats (set (range (apply min seat-ids) (+ (apply max seat-ids) 1)))]
+    (println
+      (apply min (set/difference all-seats (set seat-ids))))))
--- test/advent_of_code_2020/day5_test.clj
+++ test/advent_of_code_2020/day5_test.clj
@@ -2,12 +2,21 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2020.day5 :refer :all]))
 
+(deftest get-seat-id-test
+  (testing "Seat IDs"
+    (is (= (get-seat-id "BFFFBBFRRR")
+           567))
+    (is (= (get-seat-id "FFFBBBFRRR")
+           119))
+    (is (= (get-seat-id "BBFFBBFRLL")
+           820))))
+
 (deftest integration-test-main-1
   (testing "Integration test part 1"
-    (is (= "\n"
+    (is (= "6\n"
            (with-out-str (main-1))))))
 
 (deftest integration-test-main-2
   (testing "Integration test part 2"
-    (is (= "\n"
+    (is (= "5\n"
            (with-out-str (main-2))))))
