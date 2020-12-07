e985a64166806df24e4ab31da6298578d76a37bd
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-07 00:40:51 -0600

    Day 7: Initial solution

--- src/advent_of_code_2020/day7.clj
+++ src/advent_of_code_2020/day7.clj
@@ -1,15 +1,71 @@
 (ns advent-of-code-2020.day7
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.set :as set]))
 
 (def day-7-input (io/resource "day7.txt"))
 
+(defn parse-rule [rule]
+  (let [[bag-type contained-strings] (str/split rule #" bags contain ")
+        initial-rule {bag-type []}]
+    (if (= contained-strings "no other bags.")
+      initial-rule
+      (reduce
+        (fn [result contained-string]
+          (let [[_ quantity contained-color]
+                (re-find #"([0-9]+) (.*) bag" contained-string)]
+            (update result bag-type
+                    #(conj
+                       %
+                       {:color    contained-color
+                        :quantity (Integer/parseInt quantity)})
+                    )))
+        initial-rule
+        (str/split contained-strings #", ")))))
+
+(defn parse-rules [rules-string]
+  (reduce merge (map parse-rule (str/split-lines rules-string))))
+
+(defn get-can-contain
+  [rules-map color]
+  (let [direct-containers
+        (map
+          key
+          (filter (fn [[_ v]] (some #(= (:color %) color) v)) rules-map))]
+    (set/union (set direct-containers)
+               (reduce
+                 #(set/union
+                    %1
+                    (get-can-contain rules-map %2)) #{} direct-containers))))
+
+(defn get-bags-contained
+  [rules-map bag-type & {:keys [multiplier] :or {multiplier 1}}]
+  (reduce
+    (fn [bag-map {color :color quantity :quantity}]
+      (merge-with
+        +
+        (update bag-map color
+                (fn [old-value]
+                  (if (nil? old-value)
+                    (* quantity multiplier)
+                    (+ old-value (* quantity multiplier)))))
+        (get-bags-contained
+          rules-map color :multiplier (* multiplier quantity))))
+    {}
+    (get rules-map bag-type))
+  )
+
 (defn main-1
   []
-  (println
-    (slurp day-7-input)))
+  (let [rules-map (parse-rules (slurp day-7-input))]
+    (println
+      (count (get-can-contain rules-map "shiny gold")))))
 
 (defn main-2
   []
-  (println
-    (slurp day-7-input)))
+  (let [rules-map (parse-rules (slurp day-7-input))]
+    (println
+      (reduce
+        (fn [sum [_ v]] (+ sum v))
+        0
+        (get-bags-contained rules-map "shiny gold")))))
--- test/advent_of_code_2020/day7_test.clj
+++ test/advent_of_code_2020/day7_test.clj
@@ -1,13 +1,61 @@
-(ns advent-of-code-2020.day7_test
+(ns advent-of-code-2020.day7-test
   (:require [clojure.test :refer :all]
             [advent-of-code-2020.day7 :refer :all]))
 
+(deftest parse-rule-test
+  (testing "Parse bag containing one other bag type"
+    (is (= (parse-rule "bright white bags contain 1 shiny gold bag.")
+           {"bright white"
+            [{:color "shiny gold" :quantity 1}]})))
+  (testing "Parse bag containing multiple other bag type"
+    (is (= (parse-rule "shiny gold bags contain 1 dark olive bag, 1 vibrant plum bag.")
+           {"shiny gold"
+            [{:color "dark olive" :quantity 1}
+             {:color "vibrant plum" :quantity 1}]})))
+  (testing "Parse bag containing multiple other bag type with larger quantities"
+    (is (= (parse-rule "shiny gold bags contain 20 dark olive bags, 10 vibrant plum bags.")
+           {"shiny gold"
+            [{:color "dark olive" :quantity 20}
+             {:color "vibrant plum" :quantity 10}]})))
+  (testing "Parse bag containing no other bags"
+    (is (= (parse-rule "faded blue bags contain no other bags.")
+           {"faded blue" []}))))
+
+(let [test-rules (parse-rules "light red bags contain 1 bright white bag, 2 muted yellow bags.\ndark orange bags contain 3 bright white bags, 4 muted yellow bags.\nbright white bags contain 1 shiny gold bag.\nmuted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\nshiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\ndark olive bags contain 3 faded blue bags, 4 dotted black bags.\nvibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\nfaded blue bags contain no other bags.\ndotted black bags contain no other bags.\n")]
+  (deftest get-can-contain-test
+    (testing "Shows colors that can contain this color at 1 level"
+      (is (= (get-can-contain
+               test-rules
+               "shiny gold")
+             #{"bright white" "muted yellow" "dark orange" "light red"}))))
+  (deftest get-bags-contained-test
+    (testing "Shows empty for bottom level bag"
+      (is (= (get-bags-contained test-rules "dotted black")
+             {})))
+    (testing "Shows first level pass for bag with level-1 rule"
+      (is (= (get-bags-contained test-rules "dark olive")
+             {"faded blue"   3
+              "dotted black" 4})))
+    (testing "Shows bags that need to be nested"
+      (is (= (get-bags-contained test-rules "shiny gold")
+             {"dark olive"   1
+              "vibrant plum" 2
+              "faded blue"   13
+              "dotted black" 16})))
+    (testing "Shows combined multipliers"
+      (is (= (get-bags-contained test-rules "muted yellow")
+             {"shiny gold"   2,
+              "dark olive"   2,
+              "faded blue"   35,
+              "dotted black" 32,
+              "vibrant plum" 4})))))
+
 (deftest integration-test-main-1
   (testing "Integration test part 1"
-    (is (= "\n"
+    (is (= "4\n"
            (with-out-str (main-1))))))
 
 (deftest integration-test-main-2
   (testing "Integration test part 2"
-    (is (= "\n"
+    (is (= "32\n"
            (with-out-str (main-2))))))
