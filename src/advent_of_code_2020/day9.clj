c513037a8ce93baafa73a2cc55d770469df554bf
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-08 23:47:16 -0600

    Day 9: Initial solution

--- src/advent_of_code_2020/day9.clj
+++ src/advent_of_code_2020/day9.clj
@@ -2,14 +2,52 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(def day-9-input (io/resource "day9.txt"))
+(def day-9-input (str/split-lines (slurp (io/resource "day9.txt"))))
+
+(defn has-numbers-summing-to?
+  [result number-set]
+  (not
+    (empty?
+      (filter
+        (fn [n]
+          (contains? (disj number-set n) (- result n)))
+        number-set))))
+
+
+(defn find-invalid-entry
+  [number-vector preamble-size]
+  (first (keep-indexed
+           (fn [idx item]
+             (if (and (>= idx preamble-size)
+                      (let [subvector (subvec number-vector (- idx preamble-size) idx)]
+                        (not
+                          (has-numbers-summing-to?
+                            item
+                            (set
+                              subvector)))))
+               item))
+           number-vector)))
 
 (defn main-1
-  []
-  (println
-    (slurp day-9-input)))
+  ([] (main-1 25))
+  ([preamble-size]
+   (let
+     [numbers (vec (map bigint day-9-input))]
+     (println
+       (find-invalid-entry numbers preamble-size)))))
 
 (defn main-2
-  []
-  (println
-    (slurp day-9-input)))
+  ([] (main-2 25))
+  ([preamble-size]
+   (let
+     [numbers (vec (map bigint day-9-input))
+      invalid-entry (find-invalid-entry numbers preamble-size)
+      range-nums (loop [start-idx 0
+                   end-idx 0]
+              (let [subvector (subvec numbers start-idx end-idx)
+                    sum-in-range (reduce + subvector)]
+                (cond (< sum-in-range invalid-entry) (recur start-idx (+ end-idx 1))
+                      (> sum-in-range invalid-entry) (recur (+ start-idx 1) end-idx)
+                      :else subvector)))]
+     (println
+       (+ (reduce min range-nums) (reduce max range-nums))))))
--- test/advent_of_code_2020/day9_test.clj
+++ test/advent_of_code_2020/day9_test.clj
@@ -2,12 +2,27 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2020.day9 :refer :all]))
 
+(deftest find-numbers-summing-to-test
+  (testing "Finds numbers in set that sum to number"
+    (is (true?
+           (has-numbers-summing-to? 40 #{35
+                                         20
+                                         15
+                                         25
+                                         47})))
+    (is (false?
+           (has-numbers-summing-to? 127 #{95
+                                          102
+                                          117
+                                          150
+                                          182})))))
+
 (deftest integration-test-main-1
   (testing "Integration test part 1"
-    (is (= "\n"
-           (with-out-str (main-1))))))
+    (is (= "127N\n"
+           (with-out-str (main-1 5))))))
 
 (deftest integration-test-main-2
   (testing "Integration test part 2"
-    (is (= "\n"
-           (with-out-str (main-2))))))
+    (is (= "62N\n"
+           (with-out-str (main-2 5))))))
