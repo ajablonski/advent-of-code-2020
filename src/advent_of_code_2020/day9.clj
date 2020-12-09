95c4543afa5224bd97039cb8036fbf45f6df65ef
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-09 00:00:53 -0600

    Day 9: Extract out method for finding contiguous entries summing to a total

--- src/advent_of_code_2020/day9.clj
+++ src/advent_of_code_2020/day9.clj
@@ -13,20 +13,30 @@
           (contains? (disj number-set n) (- result n)))
         number-set))))
 
-
 (defn find-invalid-entry
   [number-vector preamble-size]
-  (first (keep-indexed
-           (fn [idx item]
-             (if (and (>= idx preamble-size)
-                      (let [subvector (subvec number-vector (- idx preamble-size) idx)]
-                        (not
-                          (has-numbers-summing-to?
-                            item
-                            (set
-                              subvector)))))
-               item))
-           number-vector)))
+  (first
+    (filter
+      (fn [[previous-entry-set item]]
+        (not
+          (has-numbers-summing-to?
+            item
+            previous-entry-set)))
+      (map #(list
+              (set (subvec number-vector %1 (+ %1 preamble-size)))
+              %2)
+           (range)
+           (subvec number-vector preamble-size)))))
+
+(defn find-contiguous-entries-summing-to
+  [number-vector target-number]
+  (loop [start-idx 0
+         end-idx 0]
+    (let [subvector (subvec number-vector start-idx end-idx)
+          sum-in-range (reduce + subvector)]
+      (cond (< sum-in-range target-number) (recur start-idx (+ end-idx 1))
+            (> sum-in-range target-number) (recur (+ start-idx 1) end-idx)
+            :else subvector))))
 
 (defn main-1
   ([] (main-1 25))
@@ -34,20 +44,14 @@
    (let
      [numbers (vec (map bigint day-9-input))]
      (println
-       (find-invalid-entry numbers preamble-size)))))
+       (second (find-invalid-entry numbers preamble-size))))))
 
 (defn main-2
   ([] (main-2 25))
   ([preamble-size]
    (let
      [numbers (vec (map bigint day-9-input))
-      invalid-entry (find-invalid-entry numbers preamble-size)
-      range-nums (loop [start-idx 0
-                   end-idx 0]
-              (let [subvector (subvec numbers start-idx end-idx)
-                    sum-in-range (reduce + subvector)]
-                (cond (< sum-in-range invalid-entry) (recur start-idx (+ end-idx 1))
-                      (> sum-in-range invalid-entry) (recur (+ start-idx 1) end-idx)
-                      :else subvector)))]
+      [_ invalid-entry] (find-invalid-entry numbers preamble-size)
+      range-nums (find-contiguous-entries-summing-to numbers invalid-entry)]
      (println
        (+ (reduce min range-nums) (reduce max range-nums))))))
