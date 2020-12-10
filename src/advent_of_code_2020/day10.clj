6ec5e81d9b47dd959afd7de8cf49ab30fb9637e3
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-10 00:02:26 -0600

    Day 10: Small scale solution

--- src/advent_of_code_2020/day10.clj
+++ src/advent_of_code_2020/day10.clj
@@ -5,10 +5,42 @@
 
 (def day-10-input (slurp (io/resource "day10.txt")))
 
+(defn get-joltage-differences
+  [input-adapters]
+  (let [differences (map (fn [input output] (- output input))
+                         (sort (conj input-adapters 0))
+                         (sort (conj input-adapters (+ (apply max input-adapters) 3))))]
+    (map-vals count (group-by identity differences))))
+
+(defn get-arrangements
+  [starting-item remaining-items result-so-far]
+  (let [next-item-possibilities (filter #(<= % (+ starting-item 3)) remaining-items)]
+    (if (empty? remaining-items)
+      (list (reverse (cons starting-item result-so-far)))
+      (mapcat (fn [next-item]
+             (get-arrangements next-item
+                               (filter #(> % next-item) remaining-items)
+                               (cons starting-item result-so-far)))
+           next-item-possibilities))))
+
+(defn get-arrangements-wrapper
+  [input-adapters]
+  (let [sorted-adapters (sort
+                          (conj
+                            (conj input-adapters 0)
+                            (+ (apply max input-adapters) 3)))
+        ]
+    (get-arrangements (first sorted-adapters) (rest sorted-adapters) '())))
+
+
+
 (defn main-1
   []
-  (println day-10-input))
+  (let [ints (map #(Integer/parseInt %) (str/split-lines day-10-input))
+        diffs (get-joltage-differences (set ints))]
+    (println (* (get diffs 3) (get diffs 1)))))
 
 (defn main-2
   []
-  (println day-10-input))
+  (let [ints (map #(Integer/parseInt %) (str/split-lines day-10-input))]
+    (println (count (get-arrangements-wrapper ints)))))
--- test/advent_of_code_2020/day10_test.clj
+++ test/advent_of_code_2020/day10_test.clj
@@ -2,12 +2,42 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2020.day10 :refer :all]))
 
+(deftest get-joltage-differences-test
+  (testing "Should calculate differences"
+    (is (= {1 2 2 1 3 3}
+           (get-joltage-differences #{1 2 5 7 10})))))
+
+(deftest get-arrangements-test
+  (testing "Should get all possible arrangements"
+    (is (= #{
+             (list 0 2 5)}
+           (set (get-arrangements-wrapper (list 2))
+           )))
+    (is (= #{
+             (list 0 1 2 5)
+             (list 0 2 5)}
+           (set (get-arrangements-wrapper (list 1 2))
+           )))
+    (is (= #{
+             (list 0, 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, 22)
+             (list 0, 1, 4, 5, 6, 7, 10, 12, 15, 16, 19, 22)
+             (list 0, 1, 4, 5, 7, 10, 11, 12, 15, 16, 19, 22)
+             (list 0, 1, 4, 5, 7, 10, 12, 15, 16, 19, 22)
+             (list 0, 1, 4, 6, 7, 10, 11, 12, 15, 16, 19, 22)
+             (list 0, 1, 4, 6, 7, 10, 12, 15, 16, 19, 22)
+             (list 0, 1, 4, 7, 10, 11, 12, 15, 16, 19, 22)
+             (list 0, 1, 4, 7, 10, 12, 15, 16, 19, 22)
+             }
+           (set (get-arrangements-wrapper (list 16 10 15 5 1 11 7 19 6 12 4)))))
+
+    ))
+
 (deftest integration-test-main-1
   (testing "Integration test part 1"
-    (is (= "\n"
+    (is (= "35\n"
            (with-out-str (main-1))))))
 
 (deftest integration-test-main-2
   (testing "Integration test part 2"
-    (is (= "\n"
+    (is (= "8\n"
            (with-out-str (main-2))))))
