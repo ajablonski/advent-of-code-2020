b74ac3549641a5ff898436f8b41c20b61e64e2a7
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-11 01:07:55 -0600

    Day 11: Solution to part 2

--- src/advent_of_code_2020/day11.clj
+++ src/advent_of_code_2020/day11.clj
@@ -43,21 +43,139 @@
        0)
     ))
 
-(defn step
+
+
+(defn step-1
   [seat-grid]
   (vec
     (map-indexed
-    (fn [row-num row]
-      (vec (map-indexed
-        (fn [col-num letter] (cond (and (= letter \#)
-                                        (too-busy? seat-grid row-num col-num))
-                                   \L
-                                   (and (= letter \L)
-                                        (others-empty? seat-grid row-num col-num))
-                                   \#
-                                   :else letter))
-        row)))
-    seat-grid)))
+      (fn [row-num row]
+        (vec (map-indexed
+               (fn [col-num letter] (cond (and (= letter \#)
+                                               (too-busy? seat-grid row-num col-num))
+                                          \L
+                                          (and (= letter \L)
+                                               (others-empty? seat-grid row-num col-num))
+                                          \#
+                                          :else letter))
+               row)))
+      seat-grid)))
+
+
+(defn get-first-visible-in-line
+  [seats]
+  (reduce
+    (fn [vis seat] (if (= vis \.) seat vis))
+    \.
+    seats))
+
+(defn get-rows
+  [grid anchor-row anchor-col]
+  (list
+    (vec (reverse (subvec (get grid anchor-row) 0 (max 0 anchor-col))))
+    (subvec (get grid anchor-row) (+ anchor-col 1))))
+
+(defn get-cols
+  [grid anchor-row anchor-col]
+  (list
+    (vec (reverse (map #(get % anchor-col) (subvec grid 0 (max 0 anchor-row)))))
+    (map #(get % anchor-col) (subvec grid (+ anchor-row 1)))))
+
+
+(defn get-diagonals
+  [grid anchor-row anchor-col]
+  (let [max-col (- (count (first grid)) 1)
+        max-row (- (count grid) 1)]
+    (list
+      (loop
+        [items '()
+         curr-row (- anchor-row 1)
+         curr-col (+ anchor-col 1)]
+        (if (or (< curr-row 0)
+                (> curr-col max-col))
+          (reverse items)
+          (recur
+            (cons
+              (get-in grid [curr-row curr-col]) items)
+            (- curr-row 1)
+            (+ curr-col 1)))
+        )
+      (loop
+        [items '()
+         curr-row (+ anchor-row 1)
+         curr-col (+ anchor-col 1)]
+        (if (or (> curr-row max-row)
+                (> curr-col max-col))
+          (reverse items)
+          (recur
+            (cons
+              (get-in grid [curr-row curr-col]) items)
+            (+ curr-row 1)
+            (+ curr-col 1)))
+        )
+      (loop
+        [items '()
+         curr-row (+ anchor-row 1)
+         curr-col (- anchor-col 1)]
+        (if (or (> curr-row max-row)
+                (< curr-col 0))
+          (reverse items)
+          (recur
+            (cons
+              (get-in grid [curr-row curr-col]) items)
+            (+ curr-row 1)
+            (- curr-col 1)))
+        )
+      (loop
+        [items '()
+         curr-row (- anchor-row 1)
+         curr-col (- anchor-col 1)]
+        (if (or (< curr-row 0)
+                (< curr-col 0))
+          (reverse items)
+          (recur
+            (cons
+              (get-in grid [curr-row curr-col]) items)
+            (- curr-row 1)
+            (- curr-col 1)))
+        )
+      )
+    ))
+
+(defn too-busy-2?
+  [grid row-num col-num]
+  (let [entries (concat (get-rows grid row-num col-num)
+                        (get-cols grid row-num col-num)
+                        (get-diagonals grid row-num col-num))]
+    (>= (count (filter #(= % \#) (map get-first-visible-in-line entries)))
+        5)
+    ))
+
+(defn others-empty-2?
+  [grid row-num col-num]
+  (let [entries (concat (get-rows grid row-num col-num)
+                        (get-cols grid row-num col-num)
+                        (get-diagonals grid row-num col-num))]
+    (= (count (filter #(= % \#) (map get-first-visible-in-line entries)))
+       0)
+    ))
+
+(defn step-2
+  [seat-grid]
+  (vec
+    (map-indexed
+      (fn [row-num row]
+        (vec (map-indexed
+               (fn [col-num letter]
+                 (cond (and (= letter \#)
+                            (too-busy-2? seat-grid row-num col-num))
+                       \L
+                       (and (= letter \L)
+                            (others-empty-2? seat-grid row-num col-num))
+                       \#
+                       :else letter))
+               row)))
+      seat-grid)))
 
 
 
@@ -70,25 +188,28 @@
       grid))
   )
 
+(defn print-grid
+  [grid]
+  (println "GRID")
+  (doseq [row grid] (println (apply str row))))
+
 (defn stabilize
-  [seat-grid]
-  (let [next-grid (step seat-grid)]
+  [seat-grid step-fn]
+  (let [next-grid (step-fn seat-grid)]
     (if (= next-grid seat-grid)
       seat-grid
-      (recur next-grid))))
+      (recur next-grid step-fn))))
 
 
 (defn main-1
   []
   (println
-    (reduce
-      +
-      (map
-        (fn [row] (count (filter #(= % \#) row)))
-        (stabilize (parse-seat-grid day-11-input))))))
+    (count-occupied (stabilize (parse-seat-grid day-11-input) step-1))))
+
 
 (defn main-2
   []
-  (println day-11-input))
+  (println
+    (count-occupied (stabilize (parse-seat-grid day-11-input) step-2))))
 
       
--- test/advent_of_code_2020/day11_test.clj
+++ test/advent_of_code_2020/day11_test.clj
@@ -10,19 +10,127 @@
 (deftest step-test
   (testing "Should step forward one state when only rule 1 applies"
     (is (= [[\. \. \.] [\. \# \.] [\. \. \.]]
-           (step ["..." ".L." "..."])))
+           (step-1 ["..." ".L." "..."])))
     (is (= [[\. \. \.] [\. \# \.] [\. \. \.]]
-           (step (step ["..." ".L." "..."]))))
+           (step-1 (step-1 ["..." ".L." "..."]))))
     (is (= [[\. \# \.] [\# \# \#] [\. \. \.]]
-           (step [".L." "LLL" "..."])))
+           (step-1 [".L." "LLL" "..."])))
     (is (= [[\. \# \.] [\# \# \#] [\. \# \.]]
-           (step [".L." "LLL" ".L."])))
+           (step-1 [".L." "LLL" ".L."])))
     (is (= [[\. \# \.] [\# \L \#] [\. \# \.]]
-           (step [[\. \# \.] [\# \# \#] [\. \# \.]])))
+           (step-1 [[\. \# \.] [\# \# \#] [\. \# \.]])))
     (is (= [[\. \# \.] [\# \L \#] [\. \# \.]]
-           (step [[\. \# \.] [\# \L \#] [\. \# \.]])))
+           (step-1 [[\. \# \.] [\# \L \#] [\. \# \.]])))
     ))
 
+
+(deftest step-2-test
+  (testing "second step function"
+    (is (= [[\# \# \L]
+            [\# \L \L]
+            [\L \L \#]]
+           (step-2 [[\# \# \L]
+                    [\# \L \L]
+                    [\L \L \L]])))
+    (let [start [[\# \. \# \# \. \# \# \. \# \#]
+                 [\# \# \# \# \# \# \# \. \# \#]
+                 [\# \. \# \. \# \. \. \# \. \.]
+                 [\# \# \# \# \. \# \# \. \# \#]
+                 [\# \. \# \# \. \# \# \. \# \#]
+                 [\# \. \# \# \# \# \# \. \# \#]
+                 [\. \. \# \. \# \. \. \. \. \.]
+                 [\# \# \# \# \# \# \# \# \# \#]
+                 [\# \. \# \# \# \# \# \# \. \#]
+                 [\# \. \# \# \# \# \# \. \# \#]]]
+      (is (= [
+              [\# \. \L \L \. \L \L \. \L \# ]
+              [\# \L \L \L \L \L \L \. \L \L ]
+              [\L \. \L \. \L \. \. \L \. \. ]
+              [\L \L \L \L \. \L \L \. \L \L ]
+              [\L \. \L \L \. \L \L \. \L \L ]
+              [\L \. \L \L \L \L \L \. \L \L ]
+              [\. \. \L \. \L \. \. \. \. \. ]
+              [\L \L \L \L \L \L \L \L \L \# ]
+              [\# \. \L \L \L \L \L \L \. \L ]
+              [\# \. \L \L \L \L \L \. \L \# ]
+              ]
+             (step-2 start))))
+    (is (= (parse-seat-grid
+             "#.L#.##.L#\n#L#####.LL\nL.#.#..#..\n##L#.##.##\n#.##.#L.##\n#.#####.#L\n..#.#.....\nLLL####LL#\n#.L#####.L\n#.L####.L#")
+           (step-2 (parse-seat-grid "#.LL.LL.L#\n#LLLLLL.LL\nL.L.L..L..\nLLLL.LL.LL\nL.LL.LL.LL\nL.LLLLL.LL\n..L.L.....\nLLLLLLLLL#\n#.LLLLLL.L\n#.LLLLL.L#"))))
+
+    ))
+
+
+
+(deftest get-first-visible-in-line-test
+  (testing "Should return the first visible seat type"
+    (is (= \.
+           (get-first-visible-in-line [\. \. \.])))
+    (is (= \L
+           (get-first-visible-in-line [\. \L \#])))
+    (is (= \#
+           (get-first-visible-in-line [\. \. \#])))
+    (is (= \.
+           (get-first-visible-in-line [])))
+    ))
+
+
+(deftest get-diagonals-test
+  (testing "Should get all 4 diagonals starting with upper right"
+    (is (= (list
+             '(\. \.)
+             '(\. \L)
+             '(\.)
+             '(\#)
+             )
+           (get-diagonals
+             [[\. \. \. \. \L]
+              [\# \. \. \. \L]
+              [\. \. \. \. \L]
+              [\. \. \. \. \L]
+              [\. \. \. \L \L]]
+             2 1)))
+    (is (= (list
+             '() '() '() '(\L \#)
+             )
+           (get-diagonals
+             [[\# \# \L]
+              [\# \L \L]
+              [\L \L \L]]
+             2 2)))
+
+    ))
+
+(deftest get-rows-test
+  (testing "Should get all 4 diagonals starting with upper right"
+    (is (= (list
+             '(\. \#)
+             '(\. \L)
+             )
+           (get-rows
+             [[\. \. \. \. \L]
+              [\# \. \. \. \L]
+              [\. \. \. \. \L]
+              [\. \. \. \. \L]
+              [\. \. \. \L \L]]
+             1 2)))
+    ))
+
+(deftest get-rows-test
+  (testing "Should get all 4 diagonals starting with upper right"
+    (is (= (list
+             '(\. \L)
+             '(\. \#)
+             )
+           (get-cols
+             [[\. \. \L \. \L]
+              [\# \. \. \. \L]
+              [\. \. \. \. \L]
+              [\. \. \. \. \L]
+              [\. \. \# \L \L]]
+             2 2)))
+    ))
 (deftest integration-test-main-1
   (testing "Integration test part 1"
     (is (= "37\n"
@@ -30,5 +138,5 @@
 
 (deftest integration-test-main-2
   (testing "Integration test part 2"
-    (is (= "\n"
+    (is (= "26\n"
            (with-out-str (main-2))))))
