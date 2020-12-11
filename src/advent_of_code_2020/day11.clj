dd84cd5d14f1a45bd1f0c615a377657cd982794c
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-10 23:41:16 -0600

    Day 11: Solution to part 1

--- src/advent_of_code_2020/day11.clj
+++ src/advent_of_code_2020/day11.clj
@@ -5,9 +5,87 @@
 
 (def day-11-input (slurp (io/resource "day11.txt")))
 
+(defn parse-seat-grid
+  [seat-grid-string]
+  (vec (map #(vec (seq (char-array %))) (str/split-lines seat-grid-string))))
+
+(defn too-busy?
+  [grid row-num col-num]
+  (let [grid-vec (vec (map vec grid))
+        entries (list
+                  (get-in grid-vec [(- row-num 1) (- col-num 1)])
+                  (get-in grid-vec [(- row-num 1) col-num])
+                  (get-in grid-vec [(- row-num 1) (+ col-num 1)])
+                  (get-in grid-vec [row-num (- col-num 1)])
+                  (get-in grid-vec [row-num (+ col-num 1)])
+                  (get-in grid-vec [(+ row-num 1) (- col-num 1)])
+                  (get-in grid-vec [(+ row-num 1) col-num])
+                  (get-in grid-vec [(+ row-num 1) (+ col-num 1)])
+                  )]
+    (>= (count (filter #(= % \#) entries))
+        4)
+    ))
+
+(defn others-empty?
+  [grid row-num col-num]
+  (let [grid-vec (vec (map vec grid))
+        entries (list
+                  (get-in grid-vec [(- row-num 1) (- col-num 1)])
+                  (get-in grid-vec [(- row-num 1) col-num])
+                  (get-in grid-vec [(- row-num 1) (+ col-num 1)])
+                  (get-in grid-vec [row-num (- col-num 1)])
+                  (get-in grid-vec [row-num (+ col-num 1)])
+                  (get-in grid-vec [(+ row-num 1) (- col-num 1)])
+                  (get-in grid-vec [(+ row-num 1) col-num])
+                  (get-in grid-vec [(+ row-num 1) (+ col-num 1)])
+                  )]
+    (= (count (filter #(= % \#) entries))
+       0)
+    ))
+
+(defn step
+  [seat-grid]
+  (vec
+    (map-indexed
+    (fn [row-num row]
+      (vec (map-indexed
+        (fn [col-num letter] (cond (and (= letter \#)
+                                        (too-busy? seat-grid row-num col-num))
+                                   \L
+                                   (and (= letter \L)
+                                        (others-empty? seat-grid row-num col-num))
+                                   \#
+                                   :else letter))
+        row)))
+    seat-grid)))
+
+
+
+(defn count-occupied
+  [grid]
+  (reduce
+    +
+    (map
+      (fn [row] (count (filter #(= % \#) row)))
+      grid))
+  )
+
+(defn stabilize
+  [seat-grid]
+  (let [next-grid (step seat-grid)]
+    (if (= next-grid seat-grid)
+      seat-grid
+      (recur next-grid))))
+
+
 (defn main-1
   []
-  (println day-11-input))
+  (println
+    (reduce
+      +
+      (map
+        (fn [row] (count (filter #(= % \#) row)))
+        (stabilize (parse-seat-grid day-11-input))))))
 
 (defn main-2
   []
--- test/advent_of_code_2020/day11_test.clj
+++ test/advent_of_code_2020/day11_test.clj
@@ -2,9 +2,30 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2020.day11 :refer :all]))
 
+(deftest parse-seat-grid-test
+  (testing "Should parse seat grid"
+    (is (= [[\L \. \.] [\# \# \#] [\. \L \.]]
+           (parse-seat-grid "L..\n###\n.L.\n")))))
+
+(deftest step-test
+  (testing "Should step forward one state when only rule 1 applies"
+    (is (= [[\. \. \.] [\. \# \.] [\. \. \.]]
+           (step ["..." ".L." "..."])))
+    (is (= [[\. \. \.] [\. \# \.] [\. \. \.]]
+           (step (step ["..." ".L." "..."]))))
+    (is (= [[\. \# \.] [\# \# \#] [\. \. \.]]
+           (step [".L." "LLL" "..."])))
+    (is (= [[\. \# \.] [\# \# \#] [\. \# \.]]
+           (step [".L." "LLL" ".L."])))
+    (is (= [[\. \# \.] [\# \L \#] [\. \# \.]]
+           (step [[\. \# \.] [\# \# \#] [\. \# \.]])))
+    (is (= [[\. \# \.] [\# \L \#] [\. \# \.]]
+           (step [[\. \# \.] [\# \L \#] [\. \# \.]])))
+    ))
+
 (deftest integration-test-main-1
   (testing "Integration test part 1"
-    (is (= "\n"
+    (is (= "37\n"
            (with-out-str (main-1))))))
 
 (deftest integration-test-main-2
