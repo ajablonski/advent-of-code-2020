179b9ec3dafc0a198d6c3a51ca2f2be2d98561f2
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-12 00:06:45 -0600

    Day 12: Initial solution

--- src/advent_of_code_2020/day12.clj
+++ src/advent_of_code_2020/day12.clj
@@ -5,12 +5,138 @@
 
 (def day-12-input (slurp (io/resource "day12.txt")))
 
+(defn move-n
+  [current-state amount]
+  (update current-state :y #(+ % amount)))
+
+(defn move-s
+  [current-state amount]
+  (update current-state :y #(- % amount)))
+
+(defn move-e
+  [current-state amount]
+  (update current-state :x #(+ % amount)))
+
+(defn move-w
+  [current-state amount]
+  (update current-state :x #(- % amount)))
+
+(let [left-turns {:N :W,
+                  :W :S,
+                  :S :E,
+                  :E :N}]
+  (defn turn-left
+    [current-state amount]
+    (if (= amount 0)
+      current-state
+      (recur
+        (update current-state :direction left-turns)
+        (- amount 90)))))
+
+(let [right-turns {:N :E,
+                   :E :S,
+                   :S :W,
+                   :W :N}]
+  (defn turn-right
+    [current-state amount]
+    (if (= amount 0)
+      current-state
+      (recur
+        (update current-state :direction right-turns)
+        (- amount 90)))))
+
+(defn move-forward
+  [current-state amount]
+  (let [direction (current-state :direction)]
+    (cond (= direction :N) (move-n current-state amount)
+          (= direction :W) (move-w current-state amount)
+          (= direction :E) (move-e current-state amount)
+          (= direction :S) (move-s current-state amount)
+          )))
+
+
+(defn move-waypoint-n
+  [current-state amount]
+  (update current-state :waypoint-y #(+ % amount)))
+
+(defn move-waypoint-s
+  [current-state amount]
+  (update current-state :waypoint-y #(- % amount)))
+
+(defn move-waypoint-e
+  [current-state amount]
+  (update current-state :waypoint-x #(+ % amount)))
+
+(defn move-waypoint-w
+  [current-state amount]
+  (update current-state :waypoint-x #(- % amount)))
+
+(defn rotate-waypoint-l
+  [current-state amount]
+  (if (= amount 0)
+    current-state
+    (recur
+      (assoc current-state :waypoint-x (- (:waypoint-y current-state))
+                           :waypoint-y (:waypoint-x current-state))
+      (- amount 90))))
+
+(defn rotate-waypoint-r
+  [current-state amount]
+  (if (= amount 0)
+    current-state
+    (recur
+      (assoc current-state :waypoint-x (:waypoint-y current-state)
+                           :waypoint-y (- (:waypoint-x current-state)))
+      (- amount 90))))
+
+(defn move-towards-waypoint
+  [current-state amount]
+  (update
+    (update current-state
+            :x #(+ % (* (:waypoint-x current-state) amount)))
+    :y #(+ % (* (:waypoint-y current-state) amount))))
+
+(defn parse-input-line
+  [line parser-map]
+  (let [[_ command amount] (re-find #"([A-Z])([0-9]+)" line)]
+    (fn [state]
+      (
+       (parser-map command)
+       state
+       (Integer/parseInt amount)))))
+
+(defn parse-input-lines
+  [input parser-map]
+  (map #(parse-input-line % parser-map) (str/split-lines input)))
+
+
+(defn get-manhattan-distance
+  [state]
+  (+ (Math/abs (:x state))
+     (Math/abs (:y state))))
+
 (defn main-1
   []
-  (println day-12-input))
+  (let [fn-map {"L" turn-left,
+                "R" turn-right,
+                "F" move-forward,
+                "N" move-n,
+                "S" move-s,
+                "E" move-e,
+                "W" move-w}
+        steps (parse-input-lines day-12-input fn-map)
+        final-pos (reduce (fn [state step] (step state)) {:x 0 :y 0 :direction :E} steps)]
+    (println (get-manhattan-distance final-pos))))
 
 (defn main-2
   []
-  (println day-12-input))
-
-      
+  (let [fn-map {"L" rotate-waypoint-l,
+                "R" rotate-waypoint-r,
+                "F" move-towards-waypoint,
+                "N" move-waypoint-n,
+                "S" move-waypoint-s,
+                "E" move-waypoint-e,
+                "W" move-waypoint-w}
+        steps (parse-input-lines day-12-input fn-map)
+        final-pos (reduce (fn [state step] (step state)) {:x 0 :y 0 :waypoint-x 10 :waypoint-y 1} steps)]
+    (println (get-manhattan-distance final-pos))))
--- test/advent_of_code_2020/day12_test.clj
+++ test/advent_of_code_2020/day12_test.clj
@@ -2,12 +2,154 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2020.day12 :refer :all]))
 
+(deftest move-n-test
+  (testing "Should move to the N"
+    (is (= ({:x 0 :y 1 :direction :W}
+            (move-n {:x 0 :y 0 :direction :W} 1))))))
+
+(deftest move-e-test
+  (testing "Should move to the E"
+    (is (= ({:x 1 :y 0 :direction :W}
+            (move-e {:x 0 :y 0 :direction :W} 1))))))
+
+(deftest move-w-test
+  (testing "Should move to the W"
+    (is (= ({:x -1 :y 0 :direction :E}
+            (move-w {:x 0 :y 0 :direction :E} 1))))))
+
+(deftest move-s-test
+  (testing "Should move to the S"
+    (is (= ({:x 0 :y -1 :direction :E}
+            (move-s {:x 0 :y 0 :direction :E} 1))))))
+
+(deftest move-f-test
+  (testing "Should move forward in current direction"
+    (is (= ({:x 0 :y 1 :direction :W}
+            (move-forward {:x 0 :y 0 :direction :N} 1))))
+    (is (= ({:x 1 :y 0 :direction :W}
+            (move-forward {:x 0 :y 0 :direction :E} 1))))
+    (is (= ({:x 0 :y -1 :direction :W}
+            (move-forward {:x 0 :y 0 :direction :S} 1))))
+    (is (= ({:x -1 :y 0 :direction :W}
+            (move-forward {:x 0 :y 0 :direction :W} 1))))
+    ))
+
+(deftest turn-l-test
+  (testing "Should turn left"
+    (is (= ({:x 0 :y 0 :direction :W}
+            (turn-left {:x 0 :y 0 :direction :N} 90))))
+    (is (= ({:x 0 :y 0 :direction :N}
+            (turn-left {:x 0 :y 0 :direction :E} 90))))
+    (is (= ({:x 0 :y 0 :direction :E}
+            (turn-left {:x 0 :y 0 :direction :S} 90))))
+    (is (= ({:x 0 :y 0 :direction :S}
+            (turn-left {:x 0 :y 0 :direction :W} 90))))
+    (is (= ({:x 0 :y 0 :direction :S}
+            (turn-left {:x 0 :y 0 :direction :N} 180))))
+    (is (= ({:x 0 :y 0 :direction :W}
+            (turn-left {:x 0 :y 0 :direction :E} 180))))
+    (is (= ({:x 0 :y 0 :direction :N}
+            (turn-left {:x 0 :y 0 :direction :S} 180))))
+    (is (= ({:x 0 :y 0 :direction :E}
+            (turn-left {:x 0 :y 0 :direction :W} 180))))
+    ))
+
+(deftest turn-r-test
+  (testing "Should turn right"
+    (is (= ({:x 0 :y 0 :direction :E}
+            (turn-right {:x 0 :y 0 :direction :N} 90))))
+    (is (= ({:x 0 :y 0 :direction :S}
+            (turn-right {:x 0 :y 0 :direction :E} 90))))
+    (is (= ({:x 0 :y 0 :direction :W}
+            (turn-right {:x 0 :y 0 :direction :S} 90))))
+    (is (= ({:x 0 :y 0 :direction :N}
+            (turn-right {:x 0 :y 0 :direction :W} 90))))
+    (is (= ({:x 0 :y 0 :direction :E}
+            (turn-right {:x 0 :y 0 :direction :W} 180))))
+    (is (= ({:x 0 :y 0 :direction :S}
+            (turn-right {:x 0 :y 0 :direction :N} 180))))
+    (is (= ({:x 0 :y 0 :direction :W}
+            (turn-right {:x 0 :y 0 :direction :E} 180))))
+    (is (= ({:x 0 :y 0 :direction :N}
+            (turn-right {:x 0 :y 0 :direction :S} 180))))
+    ))
+
+
+(deftest move-waypoint-n-test
+  (testing "Should move waypoint to the N"
+    (is (= ({:x 0 :y 0 :waypoint-x 0 :waypoint-y 1}
+            (move-waypoint-n {:x 0 :y 0 :waypoint-x 0 :waypoint-y 0} 1))))))
+
+(deftest move-waypoint-e-test
+  (testing "Should move waypoint to the E"
+    (is (= ({:x 0 :y 0 :waypoint-x 1 :waypoint-y 0}
+            (move-waypoint-e {:x 0 :y 0 :waypoint-x 0 :waypoint-y 0} 1))))))
+
+(deftest move-waypoint-w-test
+  (testing "Should move waypoint to the W"
+    (is (= ({:x 0 :y 0 :waypoint-x -1 :waypoint-y 0}
+            (move-waypoint-w {:x 0 :y 0 :waypoint-x 0 :waypoint-y 0} 1))))))
+
+(deftest move-waypoint-s-test
+  (testing "Should move waypoint to the S"
+    (is (= ({:x 0 :y 0 :waypoint-x 0 :waypoint-y -1}
+            (move-waypoint-s {:x 0 :y 0 :waypoint-x 0 :waypoint-y 0} 1))))))
+
+(deftest move-towards-waypoint-test
+  (testing "Should move forward towards waypoint"
+    (is (= ({:x 2 :y 1 :waypoint-x 2 :waypoint-y 1}
+            (move-towards-waypoint {:x 0 :y 0 :waypoint-x 2 :waypoint-y 1} 1))))
+    (is (= ({:x 6 :y 3 :waypoint-x 2 :waypoint-y 1}
+            (move-towards-waypoint {:x 0 :y 0 :waypoint-x 2 :waypoint-y 1} 3))))
+    ))
+
+(deftest rotate-waypoint-l-test
+  (testing "Should rotate waypoint left"
+    (is (= {:x 0 :y 0 :waypoint-x -4 :waypoint-y 10}
+           (rotate-waypoint-l {:x 0 :y 0 :waypoint-x 10 :waypoint-y 4} 90)))
+    (is (= {:x 0 :y 0 :waypoint-x -10 :waypoint-y -4}
+           (rotate-waypoint-l {:x 0 :y 0 :waypoint-x -4 :waypoint-y 10} 90)))
+    (is (= {:x 0 :y 0 :waypoint-x 4 :waypoint-y -10}
+           (rotate-waypoint-l {:x 0 :y 0 :waypoint-x -10 :waypoint-y -4} 90)))
+    (is (= {:x 0 :y 0 :waypoint-x 10 :waypoint-y 4}
+           (rotate-waypoint-l {:x 0 :y 0 :waypoint-x 4 :waypoint-y -10} 90)))
+    (is (= {:x 0 :y 0 :waypoint-x -10 :waypoint-y -4}
+           (rotate-waypoint-l {:x 0 :y 0 :waypoint-x 10 :waypoint-y 4} 180)))
+    (is (= {:x 0 :y 0 :waypoint-x 4 :waypoint-y -10}
+           (rotate-waypoint-l {:x 0 :y 0 :waypoint-x -4 :waypoint-y 10} 180)))
+    (is (= {:x 0 :y 0 :waypoint-x 10 :waypoint-y 4}
+           (rotate-waypoint-l {:x 0 :y 0 :waypoint-x -10 :waypoint-y -4} 180)))
+    (is (= {:x 0 :y 0 :waypoint-x -4 :waypoint-y 10}
+           (rotate-waypoint-l {:x 0 :y 0 :waypoint-x 4 :waypoint-y -10} 180)))
+    ))
+
+(deftest rotate-waypoint-r-test
+  (testing "Should rotate waypoint right"
+    (is (= {:x 0 :y 0 :waypoint-x 4 :waypoint-y -10}
+           (rotate-waypoint-r {:x 0 :y 0 :waypoint-x 10 :waypoint-y 4} 90)))
+    (is (= {:x 0 :y 0 :waypoint-x 10 :waypoint-y 4}
+           (rotate-waypoint-r {:x 0 :y 0 :waypoint-x -4 :waypoint-y 10} 90)))
+    (is (= {:x 0 :y 0 :waypoint-x -4 :waypoint-y 10}
+           (rotate-waypoint-r {:x 0 :y 0 :waypoint-x -10 :waypoint-y -4} 90)))
+    (is (= {:x 0 :y 0 :waypoint-x -10 :waypoint-y -4}
+           (rotate-waypoint-r {:x 0 :y 0 :waypoint-x 4 :waypoint-y -10} 90)))
+    (is (= {:x 0 :y 0 :waypoint-x -10 :waypoint-y -4}
+           (rotate-waypoint-r {:x 0 :y 0 :waypoint-x 10 :waypoint-y 4} 180)))
+    (is (= {:x 0 :y 0 :waypoint-x 4 :waypoint-y -10}
+           (rotate-waypoint-r {:x 0 :y 0 :waypoint-x -4 :waypoint-y 10} 180)))
+    (is (= {:x 0 :y 0 :waypoint-x 10 :waypoint-y 4}
+           (rotate-waypoint-r {:x 0 :y 0 :waypoint-x -10 :waypoint-y -4} 180)))
+    (is (= {:x 0 :y 0 :waypoint-x -4 :waypoint-y 10}
+           (rotate-waypoint-r {:x 0 :y 0 :waypoint-x 4 :waypoint-y -10} 180)))
+    ))
+
+
 (deftest integration-test-main-1
   (testing "Integration test part 1"
-    (is (= "\n"
+    (is (= "25\n"
            (with-out-str (main-1))))))
 
 (deftest integration-test-main-2
   (testing "Integration test part 2"
-    (is (= "\n"
+    (is (= "286\n"
            (with-out-str (main-2))))))
