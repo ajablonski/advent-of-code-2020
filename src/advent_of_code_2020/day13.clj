7a1e4981868b0ae153ec6be93bf80da96d257fca
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-13 00:58:45 -0600

    Day 13: Initial solution

--- src/advent_of_code_2020/day13.clj
+++ src/advent_of_code_2020/day13.clj
@@ -7,10 +7,68 @@
 
 (defn main-1
   []
-  (println day-13-input))
+  (let [[min-time routes-string] (str/split-lines day-13-input)
+        min-time-int (Integer/parseInt min-time)
+        routes (map #(Integer/parseInt %) (filter #(not (= "x" %)) (str/split routes-string #",")))
+        route-and-earliest-time-per-route
+        (map (fn [route-num]
+               (list route-num (first (filter #(>= % min-time-int)
+                                              (map #(* % route-num) (range)))))) routes)
+        [earliest-route earliest-time] (first (sort-by second route-and-earliest-time-per-route))]
+    (println (* earliest-route (- earliest-time min-time-int)))))
+
+
+(defn extended-gcd
+  "Extended GCD algorithm from
+  https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Pseudocode
+
+  Returns (GCD of a and b, s, t)
+  Such that s * a + t * b = GCD"
+  [a b]
+  (loop [old-r a
+         r b
+         old-s 1
+         s 0
+         old-t 0
+         t 1]
+    (if (= 0 r)
+      (do
+        (list old-r old-s old-t))
+      (let [quotient (quot old-r r)]
+        (recur
+          r
+          (- old-r (* quotient r))
+          s
+          (- old-s (* quotient s))
+          t
+          (- old-t (* quotient t)))))))
+
+(defn combine-phase-rotations
+  "Phase combination from https://math.stackexchange.com/a/3864593"
+  [first-period first-phase second-period second-phase]
+  (let [[gcd s _] (extended-gcd first-period second-period) ; Get GCD of the two periods,
+            ; and multiple of the first to
+        phase-diff (- first-phase second-phase) ; Total phase difference to be overcome
+        pd-mult (quot phase-diff gcd) ; phase difference is will be overcome after pd-mult periods of the GCD
+        combined-period (* second-period (quot first-period gcd)) ;
+        combined-phase (mod (- first-phase (* first-period s pd-mult)) combined-period)]
+    (list combined-period combined-phase)))
 
 (defn main-2
   []
-  (println day-13-input))
-
-      
+  (let [[_ routes-string] (str/split-lines day-13-input)
+        routes (str/split routes-string #",")
+        routes-and-offsets (map
+                             (fn [[route offset]]
+                               (list (bigint route) offset)) (filter #(not (= (first %) "x")) (map (fn [route offset] (list route offset)) routes (range))))
+        routes-times-and-offsets (map
+                                   (fn [[route offset]]
+                                     (list route
+                                      offset))
+                                   routes-and-offsets)
+        ]
+    (println (second (reduce
+      (fn [[freq-a offset-a] [freq-b offset-b]]
+        (combine-phase-rotations freq-a offset-a freq-b (mod (- offset-b) freq-b)))
+      (first routes-times-and-offsets)
+      (rest routes-times-and-offsets))))))
--- test/advent_of_code_2020/day13_test.clj
+++ test/advent_of_code_2020/day13_test.clj
@@ -2,12 +2,15 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2020.day13 :refer :all]))
 
+(deftest gcd-test
+  (testing "Greatest common divisor"))
+
 (deftest integration-test-main-1
   (testing "Integration test part 1"
-    (is (= "\n"
+    (is (= "295\n"
            (with-out-str (main-1))))))
 
 (deftest integration-test-main-2
   (testing "Integration test part 2"
-    (is (= "\n"
+    (is (= "1068781N\n"
            (with-out-str (main-2))))))
