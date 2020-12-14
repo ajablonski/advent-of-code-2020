41a74288da02f6d0fc923dad974888a1c5d72a91
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-13 19:21:58 -0600

    Day 13: Move event combining into Event protocol

--- src/advent_of_code_2020/day13.clj
+++ src/advent_of_code_2020/day13.clj
@@ -6,9 +6,21 @@
 (def day-13-input (slurp (io/resource "day13.txt")))
 
 (defprotocol EventP
-  (get-occurrences [_])
-  (get-first-departure-on-or-after [_ time])
-  (get-description [_]))
+  (get-occurrences [_]
+    "Returns an infinite series of occurrences of this event")
+  (get-first-departure-on-or-after [_ time]
+    "Get first occurrence at or after time")
+  (get-description [_]
+    "Get a description of this event using all fields")
+  (combine-with-event [this other]
+    "Given
+    - a base event (base-name) that occurs every base-period time units,
+      with first instance at base-offset (from 0)
+    - another event (other-name) that occurs every other-period time units,
+      other-offset units after base event
+
+      Returns a new event reflecting the combined event
+      (base occurring and other occurring other-offset units later)"))
 
 (defrecord Event [period offset name]
   EventP
@@ -18,7 +30,18 @@
     (first (filter #(>= % time) (get-occurrences this))))
   (get-description [_]
     (format "%s happens every %s minutes, starting at t = %s"
-            name period offset)))
+            name period offset))
+  (combine-with-event [this other]
+    (let [combined-event-occurrences
+          (filter
+            #(= 0 (mod (+ % (:offset other)) (:period other)))
+            (get-occurrences this))
+          combined-event
+          (Event. (* period (:period other))
+                  (first combined-event-occurrences)
+                  (str name " followed by " (:name other)))]
+      (println-info (get-description combined-event))
+      combined-event)))
 
 (defn parse-routes-with-initial-offsets
   [routes-string offsets]
@@ -50,32 +73,11 @@
         (- (get-first-departure-on-or-after earliest-route min-time-int)
            min-time-int)))))
 
-(defn combine-events
-  "Given
-    - a base event (base-name) that occurs every base-period time units,
-      with first instance at base-offset (from 0)
-    - another event (other-name) that occurs every other-period time units,
-      other-offset units after base event
-
-  Returns a new event reflecting the combined event
-    (base occurring and other occurring other-offset units later)"
-  [base-event other-event]
-  (let [combined-event-occurrences
-        (filter
-          #(= 0 (mod (+ % (:offset other-event)) (:period other-event)))
-          (get-occurrences base-event))
-        combined-event
-        (Event. (* (:period base-event) (:period other-event))
-                (first combined-event-occurrences)
-                (str (:name base-event) " followed by " (:name other-event)))]
-    (println-info (get-description combined-event))
-    combined-event))
-
 (defn main-2
   []
   (let [[_ routes-string] (str/split-lines day-13-input)]
     (println
       (:offset
         (reduce
-          combine-events
+          combine-with-event
           (parse-routes-with-initial-offsets routes-string (range)))))))
