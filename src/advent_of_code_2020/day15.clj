17b03f5d2a3966ffd0b15ea7f2bd65f93cbf7fa1
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-15 00:05:39 -0600

    Day 15: Initial solution

--- src/advent_of_code_2020/day15.clj
+++ src/advent_of_code_2020/day15.clj
@@ -5,12 +5,45 @@
 
 (def day-15-input (slurp (io/resource "day15.txt")))
 
+(defn expand
+  [entries-so-far last-turn last-val additional-entries]
+  (let [penultimate-spoken-index (get entries-so-far last-val)
+        curr-val (if (nil? penultimate-spoken-index) 0 (- last-turn penultimate-spoken-index))
+        ]
+    (println-debug "Turn " last-turn " Value " last-val)
+    (if (= additional-entries 0)
+      {:last-entry-map entries-so-far :last-turn last-turn :last-val last-val}
+      (recur
+        (assoc entries-so-far last-val last-turn)
+        (inc last-turn)
+        curr-val
+        (dec additional-entries)))))
+
+(defn get-value-on-turn
+  [starting-entries turn-number]
+  (let [vals-and-turns
+        (map list
+             starting-entries
+             (range 1 ##Inf))
+        [bw-last-val & bw-rest-entries] (reverse vals-and-turns)
+        [last-val last-turn] bw-last-val
+        penultimate-map (apply hash-map (flatten (reverse bw-rest-entries)))]
+    (println-debug "Initial map " penultimate-map)
+    (println-debug "Starting with turn " last-turn " having val" last-val)
+    (:last-val (expand penultimate-map last-turn last-val (- turn-number (count vals-and-turns))))))
+
 (defn main-1
   []
-  (println day-15-input))
+  (let [initial-vals (map #(Integer/parseInt (str/trim-newline %)) (str/split day-15-input #","))]
+    (println
+      (get-value-on-turn
+        initial-vals
+        2020))))
 
 (defn main-2
   []
-  (println day-15-input))
-
-      
+  (let [initial-vals (map #(Integer/parseInt (str/trim-newline %)) (str/split day-15-input #","))]
+    (println
+      (get-value-on-turn
+        initial-vals
+        30000000))))
--- test/advent_of_code_2020/day15_test.clj
+++ test/advent_of_code_2020/day15_test.clj
@@ -2,12 +2,30 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2020.day15 :refer :all]))
 
+(deftest expand-entries-test
+  (testing "Expand entries when last is new"
+    (is (= {:last-entry-map
+              {0 1
+               1 2
+               2 3} :last-turn 4 :last-val 0}
+           (expand {0 1
+                    1 2} 3 2 1))))
+  (testing "Expand entries when last is not new"
+    (is (= {':last-entry-map
+           {0 4
+            1 2
+            2 3} :last-turn 5 :last-val 3}
+           (expand {0 1
+                    1 2
+                    2 3} 4 0 1))))
+)
+
 (deftest integration-test-main-1
   (testing "Integration test part 1"
-    (is (= "\n"
+    (is (= "436\n"
            (with-out-str (main-1))))))
 
 (deftest integration-test-main-2
   (testing "Integration test part 2"
-    (is (= "\n"
+    (is (= "175594\n"
            (with-out-str (main-2))))))
