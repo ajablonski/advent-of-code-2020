d0abb129b9c6d5620823f950d7c1a25cb60d1674
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-15 01:02:47 -0600

    Day 15: Count up to target instead of down to 0, improve performance with transient map

--- src/advent_of_code_2020/day15.clj
+++ src/advent_of_code_2020/day15.clj
@@ -5,22 +5,32 @@
 
 (def day-15-input (slurp (io/resource "day15.txt")))
 
+(defn parse-input
+  [input-str]
+  (map #(Integer/parseInt (str/trim-newline %)) (str/split input-str #",")))
+
+(defmacro maybe-log-turn
+  [turn val]
+  `(if (= 0 (mod ~turn 100000))
+     (println-info "Turn " ~turn " Value " ~val)
+     (println-debug "Turn " ~turn " Value " ~val)))
+
 (defn expand
-  [entries-so-far last-turn last-val additional-entries]
-  (let [penultimate-spoken-index (get entries-so-far last-val)
-        curr-val (if (nil? penultimate-spoken-index) 0 (- last-turn penultimate-spoken-index))
-        ]
-    (println-debug "Turn " last-turn " Value " last-val)
-    (if (= additional-entries 0)
-      {:last-entry-map entries-so-far :last-turn last-turn :last-val last-val}
+  [entries-so-far last-turn last-val target-turn]
+  (loop
+    [last-turn last-turn
+     last-val last-val
+     entries-so-far (transient entries-so-far)]
+    (maybe-log-turn last-turn last-val)
+    (if (= target-turn last-turn)
+      {:last-entry-map (persistent! entries-so-far) :last-turn last-turn :last-val last-val}
       (recur
-        (assoc entries-so-far last-val last-turn)
         (inc last-turn)
-        curr-val
-        (dec additional-entries)))))
+        (- last-turn (get entries-so-far last-val last-turn))
+        (assoc! entries-so-far last-val last-turn)))))
 
 (defn get-value-on-turn
-  [starting-entries turn-number]
+  [starting-entries target-turn]
   (let [vals-and-turns
         (map list
              starting-entries
@@ -30,11 +40,11 @@
         penultimate-map (apply hash-map (flatten (reverse bw-rest-entries)))]
     (println-debug "Initial map " penultimate-map)
     (println-debug "Starting with turn " last-turn " having val" last-val)
-    (:last-val (expand penultimate-map last-turn last-val (- turn-number (count vals-and-turns))))))
+    (:last-val (expand penultimate-map last-turn last-val target-turn))))
 
 (defn main-1
   []
-  (let [initial-vals (map #(Integer/parseInt (str/trim-newline %)) (str/split day-15-input #","))]
+  (let [initial-vals (parse-input day-15-input)]
     (println
       (get-value-on-turn
         initial-vals
@@ -42,7 +52,7 @@
 
 (defn main-2
   []
-  (let [initial-vals (map #(Integer/parseInt (str/trim-newline %)) (str/split day-15-input #","))]
+  (let [initial-vals (parse-input day-15-input)]
     (println
       (get-value-on-turn
         initial-vals
--- test/advent_of_code_2020/day15_test.clj
+++ test/advent_of_code_2020/day15_test.clj
@@ -1,31 +1,31 @@
 (ns advent-of-code-2020.day15-test
   (:require [clojure.test :refer :all]
-            [advent-of-code-2020.day15 :refer :all]))
+            [advent-of-code-2020.day15 :refer :all]
+            [advent-of-code-2020.utils :refer (with-minimal-output)]))
 
 (deftest expand-entries-test
   (testing "Expand entries when last is new"
     (is (= {:last-entry-map
-              {0 1
-               1 2
-               2 3} :last-turn 4 :last-val 0}
+            {0 1
+             1 2
+             2 3} :last-turn 4 :last-val 0}
            (expand {0 1
-                    1 2} 3 2 1))))
+                    1 2} 3 2 4))))
   (testing "Expand entries when last is not new"
-    (is (= {':last-entry-map
-           {0 4
-            1 2
-            2 3} :last-turn 5 :last-val 3}
+    (is (= {:last-entry-map
+            {0 4
+             1 2
+             2 3} :last-turn 5 :last-val 3}
            (expand {0 1
                     1 2
-                    2 3} 4 0 1))))
-)
+                    2 3} 4 0 5)))))
 
 (deftest integration-test-main-1
   (testing "Integration test part 1"
     (is (= "436\n"
-           (with-out-str (main-1))))))
+           (with-out-str (with-minimal-output (main-1)))))))
 
 (deftest integration-test-main-2
   (testing "Integration test part 2"
     (is (= "175594\n"
-           (with-out-str (main-2))))))
+           (with-out-str (with-minimal-output (main-2)))))))
