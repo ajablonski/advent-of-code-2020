69f44047d41a1b1e96e3560c744f94f91021ecb8
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-16 00:46:38 -0600

    Day 16: Initial solution

--- src/advent_of_code_2020/day16.clj
+++ src/advent_of_code_2020/day16.clj
@@ -1,16 +1,129 @@
 (ns advent-of-code-2020.day16
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [advent-of-code-2020.utils :refer :all]))
+            [advent-of-code-2020.utils :refer :all]
+            [clojure.set :as set]))
 
 (def day-16-input (slurp (io/resource "day16.txt")))
 
+(defprotocol RuleP
+  (matches? [_ value]
+    "Returns true if a value is permitted by this rule"))
+
+(defrecord Rule [name range-1 range-2]
+  RuleP
+  (matches? [_ value]
+    (let [[min1 max1] range-1
+          [min2 max2] range-2]
+      (or (<= min1 value max1)
+          (<= min2 value max2)))))
+
+(defn parse-your-ticket-section
+  [your-ticket-section]
+  (let [[_ your-ticket] (str/split-lines your-ticket-section)]
+    (map #(Integer/parseInt %) (str/split your-ticket #","))))
+
+(defn parse-other-ticket-section
+  [other-ticket-section]
+  (let [[_ & rest] (str/split-lines other-ticket-section)]
+    (map (fn [ticket-line] (map #(Integer/parseInt %) (str/split ticket-line #","))) rest)))
+
+(defn parse-rules-section
+  [rules-section]
+  (map (fn
+         [rule-line]
+         (let [[_ name min-1 max-1 min-2 max-2] (re-find #"([a-z ]+): (\d+)-(\d+) or (\d+)-(\d+)" rule-line)]
+           (Rule. name [(Integer/parseInt min-1) (Integer/parseInt max-1)]
+                  [(Integer/parseInt min-2) (Integer/parseInt max-2)])))
+       (str/split-lines rules-section)))
+
+(defn parse-sections
+  [input-string]
+  (let [[rules-section your-ticket-section other-tickets-section] (str/split input-string #"\n\n")]
+    {
+     :rules         (parse-rules-section rules-section)
+     :your-ticket   (parse-your-ticket-section your-ticket-section)
+     :other-tickets (parse-other-ticket-section other-tickets-section)
+     }))
+
+(defn get-invalid-fields-for-ticket
+  [ticket rules]
+  (filter (fn [field]
+            (not (some (fn [rule] (matches? rule field)) rules))) ticket))
+
+(defn get-invalid-fields-for-tickets
+  [tickets rules]
+  (mapcat #(get-invalid-fields-for-ticket % rules) tickets))
+
 (defn main-1
   []
-  (println day-16-input))
+  (let [parsed-data (parse-sections day-16-input)]
+    (println (apply + (get-invalid-fields-for-tickets (:other-tickets parsed-data) (:rules parsed-data))))))
+
+(defn ticket-valid?
+  [ticket rules]
+  (empty? (get-invalid-fields-for-ticket ticket rules)))
+
+(defn get-valid-tickets
+  [tickets rules]
+  (filter #(ticket-valid? % rules) tickets))
+
+(defn get-possible-rules-for-field
+  [field rules]
+  (set (filter #(matches? % field) rules)))
+
+(defn get-possible-rules
+  [ticket rules]
+  (map #(get-possible-rules-for-field % rules) ticket))
+
+(defn assign-fields-to-ticket
+  [ticket field-rule-map]
+  (apply
+    hash-map
+    (flatten
+      (map-indexed
+        (fn [idx item]
+          (list
+            (:name (get field-rule-map idx))
+            item))
+        ticket))))
+
+(defn get-field-to-rule-assignments
+  [tickets rules]
+  (let [valid-tickets (get-valid-tickets tickets rules)
+        rules-per-tickets (map #(get-possible-rules % rules) valid-tickets)
+        rules-per-field (reduce
+                          (fn
+                            [aggregate rules-per-ticket]
+                            (map set/intersection aggregate rules-per-ticket))
+                          rules-per-tickets)
+        rules-per-field-with-index (map #(hash-map
+                                           :index %1
+                                           :rule-set %2) (range) rules-per-field)
+        rules-per-field-sorted (sort-by #(count (:rule-set %)) rules-per-field-with-index)]
+    (:field-rule-map
+      (reduce
+        (fn [{field-rule-map :field-rule-map used-rules :used-rules}
+             {index :index rule-set :rule-set}]
+          (let
+            [rule-for-index (first (set/difference rule-set used-rules))]
+            {:field-rule-map (assoc field-rule-map index rule-for-index)
+             :used-rules     (conj used-rules rule-for-index)}))
+        {:field-rule-map {}
+         :used-rules     #{}}
+        rules-per-field-sorted))))
 
 (defn main-2
   []
-  (println day-16-input))
-
-      
+  (let [{rules         :rules
+         other-tickets :other-tickets
+         your-ticket   :your-ticket} (parse-sections day-16-input)
+        rule-per-field (get-field-to-rule-assignments other-tickets rules)
+        parsed-ticket (assign-fields-to-ticket your-ticket rule-per-field)]
+    (println-info "Ticket details: " parsed-ticket)
+    (println
+      (apply
+        *
+        (keep
+          (fn [[k v]] (when (str/starts-with? k "departure") v))
+          parsed-ticket)))))
--- test/advent_of_code_2020/day16_test.clj
+++ test/advent_of_code_2020/day16_test.clj
@@ -1,13 +1,113 @@
 (ns advent-of-code-2020.day16-test
   (:require [clojure.test :refer :all]
-            [advent-of-code-2020.day16 :refer :all]))
+            [advent-of-code-2020.day16 :refer :all]
+            [advent-of-code-2020.utils :refer (with-minimal-output)])
+  (:import (advent_of_code_2020.day16 Rule)))
+
+(deftest parse-rules-section-test
+  (testing "Should return list of rules"
+    (is (= (list (Rule. "class" [1 3] [5 7])
+                 (Rule. "row" [6 11] [33 44])
+                 (Rule. "seat" [13 40] [45 50]))
+           (parse-rules-section "class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50")))))
+
+(deftest parse-your-ticket-section-test
+  (testing "Should parse my ticket"
+    (is (= (list 7,1,14)
+           (parse-your-ticket-section "your ticket:\n7,1,14")))))
+
+(deftest parse-other-ticket-section-test
+  (testing "Should return list of tickets"
+    (is (= (list '(7, 3, 47)
+                 '(40, 4, 50)
+                 '(55, 2, 20)
+                 '(38, 6, 12))
+           (parse-other-ticket-section "nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12")))))
+
+(deftest rule-test
+  (let [rule (Rule. "name" [1 5] [9 12])]
+    (testing "Should match correctly"
+      (testing "Should match in first range"
+        (is (true? (matches? rule 1)))
+        (is (true? (matches? rule 3)))
+        (is (true? (matches? rule 5))))
+      (testing "Should match in second range"
+        (is (true? (matches? rule 9)))
+        (is (true? (matches? rule 11)))
+        (is (true? (matches? rule 12))))
+      (testing "Should not match below first range"
+        (is (false? (matches? rule 0))))
+      (testing "Should not match between ranges"
+        (is (false? (matches? rule 6)))
+        (is (false? (matches? rule 7)))
+        (is (false? (matches? rule 8))))
+      (testing "Should not match above second range"
+        (is (false? (matches? rule 13)))))))
+
+(deftest parse-sections-test
+  (testing "Should parse the separate sections"
+    (with-redefs
+      [parse-rules-section (fn [body] (when (= body "A") :rules-section))
+       parse-your-ticket-section (fn [body] (when (= body "B") :your-ticket-section))
+       parse-other-ticket-section (fn [body] (when (= body "C") :other-ticket-section))]
+      (is (= {:rules         :rules-section
+              :your-ticket   :your-ticket-section
+              :other-tickets :other-ticket-section}
+             (parse-sections "A\n\nB\n\nC"))
+          )
+      )))
+
+(deftest get-valid-tickets-test
+  (testing "Should only return valid tickets"
+    (is (= (list '(7 3 47))
+           (get-valid-tickets (list '(7, 3, 47)
+                                    '(40, 4, 50)
+                                    '(55, 2, 20)
+                                    '(38, 6, 12))
+                              (list (Rule. "class" [1 3] [5 7])
+                                    (Rule. "row" [6 11] [33 44])
+                                    (Rule. "seat" [13 40] [45 50])))))))
+
+(deftest get-possible-rules-test
+  (let [class-rule (Rule. "class" [1 3] [5 7])
+        row-rule (Rule. "row" [6 11] [33 44])
+        seat-rule (Rule. "seat" [13 40] [45 50])]
+    (testing "Should return set of possible rules for each field in the ticket"
+      (is (= (list
+               #{class-rule row-rule}
+               #{class-rule}
+               #{seat-rule}
+               )
+             (get-possible-rules '(7 3 47) (list class-rule
+                                                 row-rule
+                                                 seat-rule)))))))
+
+(deftest assign-fields-to-ticket-test
+  (let [class-rule (Rule. "class" [1 3] [5 7])
+        row-rule (Rule. "row" [6 11] [33 44])
+        seat-rule (Rule. "seat" [13 40] [45 50])]
+  (testing "Should assign fields to a ticket based on known indices of rules"
+    (is (= {"class" 1
+            "row" 7
+            "seat" 14}
+           (assign-fields-to-ticket '(7 1 14)
+                                    {0 row-rule
+                                    1 class-rule
+                                    2 seat-rule})
+           )))))
 
 (deftest integration-test-main-1
   (testing "Integration test part 1"
-    (is (= "\n"
+    (is (= "71\n"
            (with-out-str (main-1))))))
 
 (deftest integration-test-main-2
   (testing "Integration test part 2"
-    (is (= "\n"
-           (with-out-str (main-2))))))
+    (is (= "21\n"
+           (with-out-str (with-minimal-output (main-2)))))))
