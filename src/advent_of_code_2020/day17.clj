c3230e9c199ebd4e5ef12ce0c19d2b0164d89d96
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-17 01:16:54 -0600

    Day 17: Factor out logic for findig the min and max of a given state in all dimensions

--- src/advent_of_code_2020/day17.clj
+++ src/advent_of_code_2020/day17.clj
@@ -16,33 +16,38 @@
               (fn [y row]
                 (map-indexed
                   (fn [x value] (list [x y 0] value))
-                  (.toCharArray row)))
+                  (.toCharArray ^String row)))
               (str/split-lines state-string)))))
+
+(defn get-xyz-mins-maxes
+  [state]
+  (let [min-x (apply min (keys state))
+        max-x (apply max (keys state))
+        min-y (apply min (keys (get state 0)))
+        max-y (apply max (keys (get state 0)))
+        min-z (apply min (keys (get-in state [0 0])))
+        max-z (apply max (keys (get-in state [0 0])))]
+    [[min-x max-x] [min-y max-y] [min-z max-z]]))
 
 (defn get-xyz-ranges
   [[min-x max-x] [min-y max-y] [min-z max-z]]
   (mapcat
-       (fn [x]
-         (mapcat
-           (fn [y]
-             (map
-               (fn [z] [x y z])
-               (range min-z (inc max-z))))
-           (range min-y (inc max-y))))
-       (range min-x (inc max-x))))
+    (fn [x]
+      (mapcat
+        (fn [y]
+          (map
+            (fn [z] [x y z])
+            (range min-z (inc max-z))))
+        (range min-y (inc max-y))))
+    (range min-x (inc max-x))))
 
 (defn expand-state
   [state]
-  (let [min-x (- (apply min (keys state)) 1)
-        max-x (+ (apply max (keys state)) 1)
-        min-y (- (apply min (keys (get state 0))) 1)
-        max-y (+ (apply max (keys (get state 0))) 1)
-        min-z (- (apply min (keys (get-in state [0 0]))) 1)
-        max-z (+ (apply max (keys (get-in state [0 0]))) 1)]
+  (let [[[min-x max-x] [min-y max-y] [min-z max-z]] (get-xyz-mins-maxes state)]
     (reduce
       (fn [new-state coordinates] (update-in new-state coordinates (fn [curr-val] (if (nil? curr-val) \. curr-val))))
       state
-      (get-xyz-ranges [min-x max-x] [min-y max-y] [min-z max-z]))))
+      (get-xyz-ranges [(dec min-x) (inc max-x)] [(dec min-y) (inc max-y)] [(dec min-z) (inc max-z)]))))
 
 (defn get-neighbor-coordinates
   [[x y z :as original-coordinates]]
@@ -51,12 +56,7 @@
 
 (defn print-grid
   [state]
-  (let [min-x (apply min (keys state))
-        max-x (apply max (keys state))
-        min-y (apply min (keys (get state 0)))
-        max-y (apply max (keys (get state 0)))
-        min-z (apply min (keys (get-in state [0 0])))
-        max-z (apply max (keys (get-in state [0 0])))]
+  (let [[[min-x max-x] [min-y max-y] [min-z max-z]] (get-xyz-mins-maxes state)]
     (mapcat
       (fn [z]
         (println "Z = " z)
@@ -76,22 +76,16 @@
 
 (defn update-state
   [state]
-  (let [min-x (apply min (keys state))
-        max-x (apply max (keys state))
-        min-y (apply min (keys (get state 0)))
-        max-y (apply max (keys (get state 0)))
-        min-z (apply min (keys (get-in state [0 0])))
-        max-z (apply max (keys (get-in state [0 0])))]
-    (reduce
-      (fn [new-state coordinates]
-        (update-in
-          new-state
-          coordinates
-          (fn [curr-val] (let [active-neighbors (count-active-neighbors state (get-neighbor-coordinates coordinates))]
-                           (cond (= \# curr-val) (if (<= 2 active-neighbors 3) \# \.)
-                                 (= \. curr-val) (if (= 3 active-neighbors) \# \.))))))
-      state
-      (get-xyz-ranges [min-x max-x] [min-y max-y] [min-z max-z]))))
+  (reduce
+    (fn [new-state coordinates]
+      (update-in
+        new-state
+        coordinates
+        (fn [curr-val] (let [active-neighbors (count-active-neighbors state (get-neighbor-coordinates coordinates))]
+                         (cond (= \# curr-val) (if (<= 2 active-neighbors 3) \# \.)
+                               (= \. curr-val) (if (= 3 active-neighbors) \# \.))))))
+    state
+    (apply get-xyz-ranges (get-xyz-mins-maxes state))))
 
 (defn step
   [state]
@@ -99,17 +93,11 @@
 
 (defn count-occupied
   [state]
-  (let [min-x (apply min (keys state))
-        max-x (apply max (keys state))
-        min-y (apply min (keys (get state 0)))
-        max-y (apply max (keys (get state 0)))
-        min-z (apply min (keys (get-in state [0 0])))
-        max-z (apply max (keys (get-in state [0 0])))]
-    (reduce
-      (fn [total-occupied coordinates]
-        (+ total-occupied (if (= \# (get-in state coordinates)) 1 0)))
-      0
-      (get-xyz-ranges [min-x max-x] [min-y max-y] [min-z max-z]))))
+  (reduce
+    (fn [total-occupied coordinates]
+      (+ total-occupied (if (= \# (get-in state coordinates)) 1 0)))
+    0
+    (apply get-xyz-ranges (get-xyz-mins-maxes state))))
 
 (defn main-1
   []
@@ -133,8 +121,20 @@
               (fn [y row]
                 (map-indexed
                   (fn [x value] (list [x y 0 0] value))
-                  (.toCharArray row)))
+                  (.toCharArray ^String row)))
               (str/split-lines state-string)))))
+
+(defn get-xyzw-mins-maxes
+  [state]
+  (let [min-x (apply min (keys state))
+        max-x (apply max (keys state))
+        min-y (apply min (keys (get state 0)))
+        max-y (apply max (keys (get state 0)))
+        min-z (apply min (keys (get-in state [0 0])))
+        max-z (apply max (keys (get-in state [0 0])))
+        min-w (apply min (keys (get-in state [0 0 0])))
+        max-w (apply max (keys (get-in state [0 0 0])))]
+    [[min-x max-x] [min-y max-y] [min-z max-z] [min-w max-w]]))
 
 (defn get-xyzw-ranges
   [[min-x max-x] [min-y max-y] [min-z max-z] [min-w max-w]]
@@ -153,62 +153,45 @@
 
 (defn expand-state-with-w
   [state]
-  (let [min-x (- (apply min (keys state)) 1)
-        max-x (+ (apply max (keys state)) 1)
-        min-y (- (apply min (keys (get state 0))) 1)
-        max-y (+ (apply max (keys (get state 0))) 1)
-        min-z (- (apply min (keys (get-in state [0 0]))) 1)
-        max-z (+ (apply max (keys (get-in state [0 0]))) 1)
-        min-w (- (apply min (keys (get-in state [0 0 0]))) 1)
-        max-w (+ (apply max (keys (get-in state [0 0 0]))) 1)]
+  (let [[[min-x max-x] [min-y max-y] [min-z max-z] [min-w max-w]] (get-xyzw-mins-maxes state)]
     (reduce
       (fn [new-state coordinates] (update-in new-state coordinates (fn [curr-val] (if (nil? curr-val) \. curr-val))))
       state
-      (get-xyzw-ranges [min-x max-x] [min-y max-y] [min-z max-z] [min-w max-w]))))
+      (get-xyzw-ranges
+        [(dec min-x) (inc max-x)]
+        [(dec min-y) (inc max-y)]
+        [(dec min-z) (inc max-z)]
+        [(dec min-w) (inc max-w)]))))
 
 (defn get-neighbor-coordinates-with-w
   [[x y z w :as original-coordinates]]
   (filter #(not (= % original-coordinates))
-          (get-xyzw-ranges [(- x 1) (+ x 1)] [(- y 1) (+ y 1)] [(- z 1) (+ z 1)] [(- w 1) (+ w 1)])))
-
+          (get-xyzw-ranges
+            [(- x 1) (+ x 1)]
+            [(- y 1) (+ y 1)]
+            [(- z 1) (+ z 1)]
+            [(- w 1) (+ w 1)])))
 
 (defn update-state-w
   [state]
-  (let [min-x (apply min (keys state))
-        max-x (apply max (keys state))
-        min-y (apply min (keys (get state 0)))
-        max-y (apply max (keys (get state 0)))
-        min-z (apply min (keys (get-in state [0 0])))
-        max-z (apply max (keys (get-in state [0 0])))
-        min-w (apply min (keys (get-in state [0 0 0])))
-        max-w (apply max (keys (get-in state [0 0 0])))]
-    (reduce
-      (fn [new-state coordinates]
-        (update-in
-          new-state
-          coordinates
-          (fn [curr-val] (let [active-neighbors (count-active-neighbors state (get-neighbor-coordinates-with-w coordinates))]
-                           (cond (= \# curr-val) (if (<= 2 active-neighbors 3) \# \.)
-                                 (= \. curr-val) (if (= 3 active-neighbors) \# \.))))))
-      state
-      (get-xyzw-ranges [min-x max-x] [min-y max-y] [min-z max-z] [min-w max-w]))))
+  (reduce
+    (fn [new-state coordinates]
+      (update-in
+        new-state
+        coordinates
+        (fn [curr-val] (let [active-neighbors (count-active-neighbors state (get-neighbor-coordinates-with-w coordinates))]
+                         (cond (= \# curr-val) (if (<= 2 active-neighbors 3) \# \.)
+                               (= \. curr-val) (if (= 3 active-neighbors) \# \.))))))
+    state
+    (apply get-xyzw-ranges (get-xyzw-mins-maxes state))))
 
 (defn count-occupied-w
   [state]
-  (let [min-x (apply min (keys state))
-        max-x (apply max (keys state))
-        min-y (apply min (keys (get state 0)))
-        max-y (apply max (keys (get state 0)))
-        min-z (apply min (keys (get-in state [0 0])))
-        max-z (apply max (keys (get-in state [0 0])))
-        min-w (apply min (keys (get-in state [0 0 0])))
-        max-w (apply max (keys (get-in state [0 0 0])))
-        ]
-    (reduce
-      (fn [total-occupied coordinates]
-        (+ total-occupied (if (= \# (get-in state coordinates)) 1 0)))
-      0
-      (get-xyzw-ranges [min-x max-x] [min-y max-y] [min-z max-z] [min-w max-w]))))
+  (reduce
+    (fn [total-occupied coordinates]
+      (+ total-occupied (if (= \# (get-in state coordinates)) 1 0)))
+    0
+    (apply get-xyzw-ranges (get-xyzw-mins-maxes state))))
 
 (defn step-w
   [state]
@@ -218,9 +201,9 @@
   []
   (let [initial-state (parse-initial-state-with-w day-17-input)]
     (println (count-occupied-w (->> initial-state
-                                  step-w
-                                  step-w
-                                  step-w
-                                  step-w
-                                  step-w
-                                  step-w)))))
+                                    step-w
+                                    step-w
+                                    step-w
+                                    step-w
+                                    step-w
+                                    step-w)))))
