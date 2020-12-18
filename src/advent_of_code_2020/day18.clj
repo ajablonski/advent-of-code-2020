981ac17bdc743cea65e97f0e9cc3b07e2db3f4bb
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-18 00:15:02 -0600

    Day 18: Initial solution

--- src/advent_of_code_2020/day18.clj
+++ src/advent_of_code_2020/day18.clj
@@ -1,16 +1,69 @@
 (ns advent-of-code-2020.day18
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [advent-of-code-2020.utils :refer :all]))
+            [advent-of-code-2020.utils :refer :all]
+            [instaparse.core :as insta]))
 
 (def day-18-input (slurp (io/resource "day18.txt")))
 
+(def parse-expression-equal-precedence
+  (insta/parser "
+    expression = term { <' '> operation <' '> term }
+    <term> = digit | <'('> expression <')'>
+    digit = #'\\d'
+    operation = '*' | '+'
+    "))
+
+(defn evaluate-expression-equal-precedence
+  ([[expr-type & rest-expr]]
+   (cond (and (= expr-type :expression) (= 1 (count rest-expr))) (apply evaluate-expression-equal-precedence rest-expr)
+         (= expr-type :expression)
+         (let [[l op r & rest] rest-expr]
+           (evaluate-expression-equal-precedence (into [:expression
+                                                        [:evaled
+                                                         ((evaluate-expression-equal-precedence op)
+                                                          (evaluate-expression-equal-precedence l)
+                                                          (evaluate-expression-equal-precedence r))]]
+                                                       rest)))
+         (= expr-type :digit) (Integer/parseInt (first rest-expr))
+         (= expr-type :operation) (resolve (symbol (first rest-expr)))
+         (= expr-type :evaled) (first rest-expr)
+         :else (do (println "Fell through at " expr-type "\nwith rest: " rest-expr)))
+   ))
+
 (defn main-1
   []
-  (println day-18-input))
+  (println
+    (apply
+      +
+      (map
+        #(evaluate-expression-equal-precedence (parse-expression-equal-precedence %))
+        (str/split-lines day-18-input)))))
+
+(def parse-expression-addition-precedence
+  (insta/parser "
+    expression = term { <' '> <'*'> <' '> term }
+    term = added { <' '> <'+'> <' '> added }
+    added = digit | <'('> expression <')'>
+    digit = #'\\d'
+    "))
+
+(defn evaluate-expression-addition-precedence
+  ([[expr-type & rest-expr]]
+   (cond (= expr-type :expression) (apply * (map evaluate-expression-addition-precedence rest-expr))
+         (= expr-type :term) (apply + (map evaluate-expression-addition-precedence rest-expr))
+         (= expr-type :added) (apply evaluate-expression-addition-precedence rest-expr)
+         (= expr-type :digit) (Integer/parseInt (first rest-expr))
+         :else (do (println "Fell through at " expr-type "\nWith rest: " rest-expr)))
+   ))
 
 (defn main-2
   []
-  (println day-18-input))
+  (println
+    (apply
+      +
+      (map
+        #(evaluate-expression-addition-precedence (parse-expression-addition-precedence %))
+        (str/split-lines day-18-input)))))
 
       
--- test/advent_of_code_2020/day18_test.clj
+++ test/advent_of_code_2020/day18_test.clj
@@ -2,12 +2,56 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2020.day18 :refer :all]))
 
+
+(deftest evaluate-expression-test
+  (testing "Should perform addition"
+    (is (= 6
+           (evaluate-expression-equal-precedence (parse-expression-equal-precedence "1 + 2 + 3")))))
+  (testing "Should perform multiplication"
+    (is (= 24
+           (evaluate-expression-equal-precedence (parse-expression-equal-precedence "1 * 2 * 3 * 4")))))
+  (testing "Should perform mix addition and multiplication"
+    (is (= 71
+           (evaluate-expression-equal-precedence (parse-expression-equal-precedence "1 + 2 * 3 + 4 * 5 + 6")))))
+  (testing "Should handle operation in parentheses"
+    (is (= 3
+           (evaluate-expression-equal-precedence (parse-expression-equal-precedence "(1 + 2)")))))
+  (testing "Should handle nested parentheses"
+    (is (= 12240
+           (evaluate-expression-equal-precedence (parse-expression-equal-precedence "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"))))
+    (is (= 13632
+           (evaluate-expression-equal-precedence (parse-expression-equal-precedence "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))))
+    )
+  )
+
+
+(deftest evaluate-expression-addition-precedence-test
+  (testing "Should perform addition"
+    (is (= 6
+           (evaluate-expression-addition-precedence (parse-expression-addition-precedence "1 + 2 + 3")))))
+  (testing "Should perform multiplication"
+    (is (= 24
+           (evaluate-expression-addition-precedence (parse-expression-addition-precedence "1 * 2 * 3 * 4")))))
+  (testing "Should perform mix addition and multiplication"
+    (is (= 231
+           (evaluate-expression-addition-precedence (parse-expression-addition-precedence "1 + 2 * 3 + 4 * 5 + 6")))))
+  (testing "Should handle operation in parentheses"
+    (is (= 3
+           (evaluate-expression-addition-precedence (parse-expression-addition-precedence "(1 + 2)")))))
+  (testing "Should handle nested parentheses"
+    (is (= 669060
+           (evaluate-expression-addition-precedence (parse-expression-addition-precedence "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"))))
+    (is (= 23340
+           (evaluate-expression-addition-precedence (parse-expression-addition-precedence "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))))
+    )
+  )
+
 (deftest integration-test-main-1
   (testing "Integration test part 1"
-    (is (= "\n"
+    (is (= "26406\n"
            (with-out-str (main-1))))))
 
 (deftest integration-test-main-2
   (testing "Integration test part 2"
-    (is (= "\n"
+    (is (= "694122\n"
            (with-out-str (main-2))))))
