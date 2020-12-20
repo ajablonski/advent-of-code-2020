82336186c8690b6373837e71b04a1a803f5a3391
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-19 18:27:21 -0600

    Day 19: Initial solution using Instaparse

--- src/advent_of_code_2020/day19.clj
+++ src/advent_of_code_2020/day19.clj
@@ -1,16 +1,28 @@
 (ns advent-of-code-2020.day19
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [advent-of-code-2020.utils :refer :all]))
+            [advent-of-code-2020.utils :refer :all]
+            [instaparse.core :as insta]))
 
 (def day-19-input (slurp (io/resource "day19.txt")))
 
 (defn main-1
   []
-  (println day-19-input))
+  (let [[rules-part vals-part] (str/split day-19-input #"\n\n")
+        parser (insta/parser (str "S: 0\n" rules-part))]
+    (println (count (filter #(not (empty? %)) (map #(insta/parses parser %) (str/split-lines vals-part)))))))
 
 (defn main-2
   []
-  (println day-19-input))
+  (let [[rules-part vals-part] (str/split day-19-input #"\n\n")
+        parser (insta/parser
+                 (str/replace
+                   (str/replace
+                     (str "S: 0\n" rules-part)
+                     "8: 42"
+                     "8: 42 | 42 8")
+                   "11: 42 31"
+                   "11: 42 31 | 42 11 31"))]
+    (println (count (filter #(not (empty? %)) (map #(insta/parses parser %) (str/split-lines vals-part)))))))
 
       
--- test/advent_of_code_2020/day19_test.clj
+++ test/advent_of_code_2020/day19_test.clj
@@ -4,10 +4,10 @@
 
 (deftest integration-test-main-1
   (testing "Integration test part 1"
-    (is (= "\n"
+    (is (= "3\n"
            (with-out-str (main-1))))))
 
 (deftest integration-test-main-2
   (testing "Integration test part 2"
-    (is (= "\n"
+    (is (= "12\n"
            (with-out-str (main-2))))))
