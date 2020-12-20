c1a7eb384d22ca49c82990c7e8706f61eb49465b
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-19 18:52:12 -0600

    Day 19: Factor out common parts of main functions

--- src/advent_of_code_2020/day19.clj
+++ src/advent_of_code_2020/day19.clj
@@ -2,27 +2,41 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
             [advent-of-code-2020.utils :refer :all]
-            [instaparse.core :as insta]))
+            [instaparse.core :as insta])
+  (:import (instaparse.gll Failure)))
 
 (def day-19-input (slurp (io/resource "day19.txt")))
 
+(defn parse-input
+  ([input-string] (parse-input input-string '()))
+  ([input-string replacements]
+   (let [[rules-part vals-part] (str/split input-string #"\n\n")]
+     (list
+       (insta/parser
+         (str "S: 0\n"
+              (reduce
+                (fn [result [replace with]] (str/replace result replace with))
+                rules-part
+                replacements)))
+       (str/split-lines vals-part)))))
+
+(defn parseable?
+  [parser]
+  (fn [input-str] (not (instance? Failure (insta/parse parser input-str)))))
+
+(defn- do-main
+  [parser inputs]
+  (count
+    (filter
+      (parseable? parser)
+      inputs)))
+
 (defn main-1
   []
-  (let [[rules-part vals-part] (str/split day-19-input #"\n\n")
-        parser (insta/parser (str "S: 0\n" rules-part))]
-    (println (count (filter #(not (empty? %)) (map #(insta/parses parser %) (str/split-lines vals-part)))))))
+  (println (apply do-main (parse-input day-19-input))))
 
 (defn main-2
   []
-  (let [[rules-part vals-part] (str/split day-19-input #"\n\n")
-        parser (insta/parser
-                 (str/replace
-                   (str/replace
-                     (str "S: 0\n" rules-part)
-                     "8: 42"
-                     "8: 42 | 42 8")
-                   "11: 42 31"
-                   "11: 42 31 | 42 11 31"))]
-    (println (count (filter #(not (empty? %)) (map #(insta/parses parser %) (str/split-lines vals-part)))))))
+  (let [replacements (list '("8: 42" "8: 42 | 42 8") '("11: 42 31" "11: 42 31 | 42 11 31"))]
+    (println (apply do-main (parse-input day-19-input replacements)))))
 
-      
