d1091131f9c713681af9e777c1975a6bef929eff
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-21 00:19:44 -0600

    Day 21: Initial solution

--- src/advent_of_code_2020/day21.clj
+++ src/advent_of_code_2020/day21.clj
@@ -1,16 +1,75 @@
 (ns advent-of-code-2020.day21
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [advent-of-code-2020.utils :refer :all]))
+            [advent-of-code-2020.utils :refer :all]
+            [clojure.set :as set]))
 
 (def day-21-input (slurp (io/resource "day21.txt")))
 
+(defn parse-food
+  [food-string]
+  (let [[_ food-part allergen-part] (re-matches #"(.*) \(contains (.*)\)" food-string)]
+    {:ingredients (set (str/split food-part #" ")) :allergens (set (str/split allergen-part #", "))}))
+
+(defn parse-foods
+  [foods-string]
+  (map parse-food (str/split-lines foods-string)))
+
+(defn get-all-allergens
+  [food-list]
+  (apply set/union (map :allergens food-list)))
+
+(defn get-possible-culprits-for-allergen
+  [allergen food-list]
+  (apply set/intersection
+         (map :ingredients (filter #(contains? (:allergens %) allergen) food-list))))
+
+(defn get-all-ingredients
+  [food-list]
+  (apply set/union (map :ingredients food-list)))
+
+(defn get-allergen-potential-ingredients
+  [food-list]
+  (let [allergens (get-all-allergens food-list)]
+    (apply set/union
+           (map #(get-possible-culprits-for-allergen % food-list) allergens))))
+
+(defn get-allergen-free-ingredients
+  [food-list]
+  (let [ingredients (get-all-ingredients food-list)]
+    (set/difference ingredients (get-allergen-potential-ingredients food-list))))
+
+(defn count-ingredient-appearances
+  [food ingredients]
+  (count (set/intersection (:ingredients food) ingredients)))
+
 (defn main-1
   []
-  (println day-21-input))
+  (let [food-list (parse-foods day-21-input)
+        allergen-free-ingredients (get-allergen-free-ingredients food-list)]
+    (println (apply + (map #(count-ingredient-appearances % allergen-free-ingredients) food-list)))))
 
 (defn main-2
   []
-  (println day-21-input))
-
+  (let [food-list (parse-foods day-21-input)
+        allergen-free-ingredients (get-allergen-free-ingredients food-list)
+        allergens (get-all-allergens food-list)
+        foods-with-only-allergen-ingredients (map #(update % :ingredients (fn [old-ingredients] (set/difference old-ingredients allergen-free-ingredients))) food-list)
+        allergens-and-ingredients (map (fn [allergen] (list allergen (get-possible-culprits-for-allergen allergen foods-with-only-allergen-ingredients))) allergens)
+        allergen-ingredient-map (loop
+                [end-map (sorted-map)
+                 known-ingredients #{}
+                 to-process allergens-and-ingredients
+                 ]
+                (println-info "End map" end-map "known ingredients" known-ingredients "to process" to-process)
+                (if (empty? to-process)
+                  end-map
+                  (let [[[a is] & rest] (sort-by #(count (second %)) to-process)
+                        ingredient (first is)]
+                    (recur
+                      (assoc end-map a ingredient)
+                      (conj known-ingredients ingredient)
+                      (map (fn [[a is]] (list a (disj is ingredient))) rest))
+                    )))]
+    (println (str/join "," (map second allergen-ingredient-map)))))
       
--- test/advent_of_code_2020/day21_test.clj
+++ test/advent_of_code_2020/day21_test.clj
@@ -1,13 +1,82 @@
 (ns advent-of-code-2020.day21-test
   (:require [clojure.test :refer :all]
-            [advent-of-code-2020.day21 :refer :all]))
+            [advent-of-code-2020.day21 :refer :all]
+            [advent-of-code-2020.utils :refer [with-minimal-output]]))
+
+(deftest parse-food-test
+  (testing "Should parse food into ingredients and allergents"
+    (is (= {:ingredients #{"mxmxvkd" "kfcds" "sqjhc" "nhms"}
+            :allergens   #{"dairy" "fish"}}
+           (parse-food "mxmxvkd kfcds sqjhc nhms (contains dairy, fish)")))))
+
+
+(deftest get-possible-allergens-test
+  (testing "Should get allergens from multiple foods"
+    (is (= #{"dairy" "fish" "soy"}
+           (get-all-allergens
+             (list
+               {:ingredients #{"mxmxvkd" "kfcds" "sqjhc" "nhms"}
+                :allergens   #{"dairy" "fish"}}
+               {:ingredients #{"adsfpo" "dosi" "eiu"}
+                :allergens   #{"soy" "fish"}}
+               ))
+           ))))
+
+(deftest get-possible-culprits-for-allergen-test
+  (testing "Should get all of the possible sources of an allergen"
+    (is (= #{"mxmxvkd" "sqjhc"}
+           (get-possible-culprits-for-allergen
+             "fish"
+             (list
+               {:ingredients #{"sqjhc" "mxmxvkd" "sbzzf"}
+                :allergens   #{"fish"}}
+               {:ingredients #{"mxmxvkd" "kfcds" "sqjhc" "nhms"}
+                :allergens   #{"dairy" "fish"}}))))))
+
+(deftest get-all-ingredients-test
+  (testing "Should return all ingredients"
+    (is (= #{"sqjhc" "mxmxvkd" "sbzzf" "nhms"}
+           (get-all-ingredients
+             (list
+               {:ingredients #{"sqjhc" "mxmxvkd" "sbzzf"}
+                :allergens   #{"fish"}}
+               {:ingredients #{"mxmxvkd" "sbzzf" "sqjhc" "nhms"}
+                :allergens   #{"dairy" "fish"}}))))))
+
+(deftest get-allergen-potential-ingredients-test
+  (testing "Should return all ingredients that may contain an allergen"
+    (is (= #{"mxmxvkd" "sqjhc" "fvjkl"}
+           (get-allergen-potential-ingredients
+             (list
+               {:ingredients #{"mxmxvkd" "kfcds" "sqjhc" "nhms"}
+                :allergens   #{"dairy" "fish"}}
+               {:ingredients #{"trh" "fvjkl" "sbzzf" "mxmxvkd"}
+                :allergens   #{"dairy"}}
+               {:ingredients #{"sqjhc" "fvjkl"}
+                :allergens   #{"soy"}}
+               {:ingredients #{"sqjhc" "mxmxvkd" "sbzzf"}
+                :allergens   #{"fish"}}))))))
+
+(deftest get-allergen-free-ingredients-test
+  (testing "Should return all ingredients that cannot contain an allergen"
+    (is (= #{"kfcds" "nhms" "sbzzf" "trh"}
+           (get-allergen-free-ingredients
+             (list
+               {:ingredients #{"mxmxvkd" "kfcds" "sqjhc" "nhms"}
+                :allergens   #{"dairy" "fish"}}
+               {:ingredients #{"trh" "fvjkl" "sbzzf" "mxmxvkd"}
+                :allergens   #{"dairy"}}
+               {:ingredients #{"sqjhc" "fvjkl"}
+                :allergens   #{"soy"}}
+               {:ingredients #{"sqjhc" "mxmxvkd" "sbzzf"}
+                :allergens   #{"fish"}}))))))
 
 (deftest integration-test-main-1
   (testing "Integration test part 1"
-    (is (= "\n"
+    (is (= "5\n"
            (with-out-str (main-1))))))
 
 (deftest integration-test-main-2
   (testing "Integration test part 2"
-    (is (= "\n"
-           (with-out-str (main-2))))))
+    (is (= "mxmxvkd,sqjhc,fvjkl\n"
+           (with-out-str (with-minimal-output (main-2)))))))
