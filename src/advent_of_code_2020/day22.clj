bc9ebde75bedcb236975ac45b9097fc248e7b79d
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-22 00:19:02 -0600

    Day 22: Initial solution

--- src/advent_of_code_2020/day22.clj
+++ src/advent_of_code_2020/day22.clj
@@ -5,12 +5,80 @@
 
 (def day-22-input (slurp (io/resource "day22.txt")))
 
+(defn- parse-player-input
+  [player-input-string]
+  (map #(Integer/parseInt %) (rest (str/split-lines player-input-string))))
+
+(defn parse-input
+  [input-string]
+  (let [[p1-part p2-part] (str/split input-string #"\n\n")]
+    (list (parse-player-input p1-part) (parse-player-input p2-part))))
+
+(defn take-normal-turn
+  [[[p1-next & p1-rest] [p2-next & p2-rest]]]
+  (if (> p1-next p2-next)
+    (list (concat p1-rest (list p1-next p2-next)) p2-rest)
+    (list p1-rest (concat p2-rest (list p2-next p1-next)))))
+
+(defn play-normal-game
+  [[p1-cards p2-cards :as hands]]
+  (if (or (empty? p1-cards) (empty? p2-cards))
+    hands
+    (recur (take-normal-turn hands))))
+
+(defn calculate-score
+  [hand]
+  (apply
+    +
+    (map *
+         hand
+         (range (count hand) 0 -1))))
+
 (defn main-1
   []
-  (println day-22-input))
+  (let [hands (play-normal-game (parse-input day-22-input))
+        winning-hand (first (filter some? hands))]
+    (println (calculate-score winning-hand))))
+
+(defn get-winner
+  [hands]
+  (if (nil? (first hands)) 2 1))
+
+(defn should-play-recursive?
+  [[[p1-next & p1-rest] [p2-next & p2-rest]]]
+  (and (>= (count p1-rest) p1-next)
+       (>= (count p2-rest) p2-next)))
+
+(declare play-recursive-game)
+
+(defn take-recursive-turn
+  [[[p1-next & p1-rest] [p2-next & p2-rest] :as hands] game]
+  (if (should-play-recursive? hands)
+    (if (=
+          (get-winner
+            (play-recursive-game
+              (list
+                (take p1-next p1-rest)
+                (take p2-next p2-rest))
+              (+ game 1)))
+          1)
+      (list (concat p1-rest (list p1-next p2-next)) p2-rest)
+      (list p1-rest (concat p2-rest (list p2-next p1-next))))
+    (take-normal-turn hands)))
+
+(defn play-recursive-game
+  [hands game]
+  (println-info (apply str (repeat (dec game) " ")) "Game" game)
+  (loop
+    [turn-history #{}
+     hands hands]
+    (cond
+      (or (empty? (first hands)) (empty? (second hands))) hands
+      (contains? turn-history hands) (first hands)
+      :else (recur (conj turn-history hands) (take-recursive-turn hands game)))))
 
 (defn main-2
   []
-  (println day-22-input))
-
-      
+  (let [hands (play-recursive-game (parse-input day-22-input) 1)
+        winning-hand (first (filter some? hands))]
+    (println (calculate-score winning-hand))))
--- test/advent_of_code_2020/day22_test.clj
+++ test/advent_of_code_2020/day22_test.clj
@@ -1,13 +1,34 @@
 (ns advent-of-code-2020.day22-test
   (:require [clojure.test :refer :all]
-            [advent-of-code-2020.day22 :refer :all]))
+            [advent-of-code-2020.day22 :refer :all]
+            [advent-of-code-2020.utils :refer [with-minimal-output]]))
+
+(deftest parse-input-test
+  (testing "Should parse input"
+    (is (= (list '(9 2 6 3 1) '(5 8 4 7 10))
+           (parse-input day-22-input)))))
+
+(deftest take-turn-test
+  (testing "Should take a turn, putting winning cards at end of winner's hand with winning card first"
+    (is (= (list '(2 6 3 1 9 5) '(8 4 7 10))
+           (take-normal-turn (list (list 9 2 6 3 1) (list 5 8 4 7 10)))))))
+
+(deftest play-game-test
+  (testing "Should play a full game, resulting in p2 winning"
+    (is (= (list nil '(3 2 10 6 8 5 9 4 7 1))
+           (play-normal-game (list '(9 2 6 3 1) '(5 8 4 7 10)))))))
+
+(deftest calculate-score-test
+  (testing "Should calculate the score using decreasing point count"
+    (is (= 306
+           (calculate-score '(3 2 10 6 8 5 9 4 7 1))))))
 
 (deftest integration-test-main-1
   (testing "Integration test part 1"
-    (is (= "\n"
+    (is (= "306\n"
            (with-out-str (main-1))))))
 
 (deftest integration-test-main-2
   (testing "Integration test part 2"
-    (is (= "\n"
-           (with-out-str (main-2))))))
+    (is (= "291\n"
+           (with-out-str (with-minimal-output (main-2)))))))
