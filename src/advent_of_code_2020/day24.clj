b8ca827a206cfc374bbd1602abd4cf8577de67fe
Alex Jablonski <alexmjablonski@gmail.com>
2020-12-24 00:07:53 -0600

    Day 24: Initial solution

--- src/advent_of_code_2020/day24.clj
+++ src/advent_of_code_2020/day24.clj
@@ -5,12 +5,137 @@
 
 (def day-24-input (slurp (io/resource "day24.txt")))
 
+(defn parse-input-line
+  [input-line]
+  (loop [letters input-line
+         result '()]
+    (if (empty? letters)
+      (reverse result)
+      (let [token-start (first letters)
+            token-end (if (contains? #{\e \w} token-start) nil (second letters))
+            next-letters (if (contains? #{\e \w} token-start) (rest letters) (drop 2 letters))]
+        (recur next-letters (cons (keyword (str token-start token-end)) result))))))
+
+(let [coord-delta-dict {:w  [-1 0]
+                        :e  [1 0]
+                        :nw [0 1]
+                        :se [0 -1]
+                        :ne [1 1]
+                        :sw [-1 -1]}]
+  (defn get-coordinate
+    [instruction-seq]
+    (loop
+      [from [0 0]
+       instructions instruction-seq]
+      (if (empty? instructions)
+        from
+        (recur
+          (map + from (get coord-delta-dict (first instructions)))
+          (rest instructions))))))
+
+(defn parse-input-lines
+  [input-lines]
+  (map parse-input-line (str/split-lines input-lines)))
+
+(defn get-tile-start-state
+  [instructions]
+  (reduce
+    (fn [state-map instruction]
+      (update-in
+        state-map
+        (get-coordinate instruction)
+        (fn
+          [old-val]
+          (if (or (nil? old-val) (= old-val :white))
+            :black
+            :white))))
+    {}
+    instructions))
+
+(defn count-black-tiles
+  [state]
+  (apply
+    +
+    (map
+      (fn [[_ col]]
+        (apply
+          +
+          (map
+            (fn [val] (if (= val :black) 1 0))
+            (vals col))))
+      state)))
+
 (defn main-1
   []
-  (println day-24-input))
+  (let [instructions (parse-input-lines day-24-input)]
+    (println
+      (count-black-tiles (get-tile-start-state instructions)))))
+
+(defn expand-grid
+  [state]
+  (let [min-x (apply min (keys state))
+        max-x (apply max (keys state))
+        min-y (apply min (mapcat keys (vals state)))
+        max-y (apply max (mapcat keys (vals state)))]
+    (reduce
+      (fn [state x]
+        (reduce
+          (fn [state y]
+            (update-in state [x y] (fn [old-val] (if (nil? old-val) :white old-val))))
+          state
+          (range (dec min-y) (+ max-y 2)))
+        )
+      state
+      (range (dec min-x) (+ max-x 2)))))
+
+(defn count-black-neighbors
+  [grid [x y]]
+  (let [neighbors (list [x (inc y)]
+                        [x (dec y)]
+                        [(inc x) y]
+                        [(dec x) y]
+                        [(inc x) (inc y)]
+                        [(dec x) (dec y)])]
+    (count (filter #(= % :black) (map (fn [coords] (get-in grid coords)) neighbors)))))
+
+(defn take-step
+  [orig-state]
+  (let [min-x (apply min (keys orig-state))
+        max-x (apply max (keys orig-state))
+        min-y (apply min (mapcat keys (vals orig-state)))
+        max-y (apply max (mapcat keys (vals orig-state)))]
+    (reduce
+      (fn [state x]
+        (reduce
+          (fn [state y]
+            (update-in
+              state
+              [x y]
+              (fn [old-val]
+                (let [black-neighbor-count
+                      (count-black-neighbors orig-state [x y])]
+                  (cond
+                    (and (= old-val :white) (= black-neighbor-count 2)) :black
+                    (and (= old-val :black) (or (= black-neighbor-count 0) (> black-neighbor-count 2))) :white
+                    :else old-val)))))
+          state
+          (range min-y (inc max-y)))
+        )
+      orig-state
+      (range min-x (inc max-x)))))
+
+(defn take-steps
+  [grid step-count]
+  (println-info "Steps left" step-count)
+  (if (= step-count 0)
+    grid
+    (recur (take-step (expand-grid grid)) (dec step-count))))
 
 (defn main-2
   []
-  (println day-24-input))
-
+  (let [instructions (parse-input-lines day-24-input)
+        init-state (get-tile-start-state instructions)
+        final-state (take-steps init-state 100)]
+    (println
+      (count-black-tiles final-state))))
       
--- test/advent_of_code_2020/day24_test.clj
+++ test/advent_of_code_2020/day24_test.clj
@@ -1,13 +1,42 @@
 (ns advent-of-code-2020.day24-test
   (:require [clojure.test :refer :all]
-            [advent-of-code-2020.day24 :refer :all]))
+            [advent-of-code-2020.day24 :refer :all]
+            [advent-of-code-2020.utils :refer [with-minimal-output]]))
+
+(deftest parse-input-line-test
+  (testing "Should parse e and w as individual tokens"
+    (is (= '(:e :w :w :e :e :w)
+           (parse-input-line "ewweew"))))
+  (testing "Should parse se, sw, ne, and nw as separate tokens"
+    (is (= '(:se :sw :ne :nw :nw :sw :ne :se)
+           (parse-input-line "seswnenwnwswnese"))))
+  (testing "Should parse a complex string"
+    (is (= '(:ne :e :e :ne :se :nw :nw :w :sw :ne :ne :w :nw :w :se :w :ne :nw :se :sw :e :sw)
+           (parse-input-line "neeenesenwnwwswnenewnwwsewnenwseswesw")))))
+
+(deftest get-coordinates-test
+  (testing "Should calculate coordinates"
+    (is (= '(-1 0) (get-coordinate '(:w))))
+    (is (= '(1 0) (get-coordinate '(:e))))
+    (is (= '(0 1) (get-coordinate '(:nw))))
+    (is (= '(0 -1) (get-coordinate '(:se))))
+    (is (= '(1 1) (get-coordinate '(:ne))))
+    (is (= '(-1 -1) (get-coordinate '(:sw))))
+    (is (= '(0 0) (get-coordinate '(:nw :w :sw :e :e))))))
+
+(deftest expand-grid-test
+  (testing "Should expand a state grid by +- 1 in x and y"
+    (is (= {-1 {-1 :white 0 :white 1 :white}
+            0 {-1 :white 0 :black 1 :white}
+            1 {-1 :white 0 :white 1 :white}}
+           (expand-grid {0 {0 :black}})))))
 
 (deftest integration-test-main-1
   (testing "Integration test part 1"
-    (is (= "\n"
+    (is (= "10\n"
            (with-out-str (main-1))))))
 
 (deftest integration-test-main-2
   (testing "Integration test part 2"
-    (is (= "\n"
-           (with-out-str (main-2))))))
+    (is (= "2208\n"
+           (with-out-str (with-minimal-output (main-2)))))))
